ba831d165a0e69d52324b70ad42639fb8fac01d5
Christopher <chrissitopher@gmx.de>
2012-05-21 22:21:20 +0200

    [TASK] Import doc_core_cgl 4.4
    Import doc_core_cgl for TYPO3 4.4.
    
    Change-Id: Id54744086a05b2d8deec4411b00bcf6a1545fd03

--- doc_core_cgl/ext_emconf.php
+++ doc_core_cgl/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "doc_core_cgl"
+# Extension Manager/Repository config file for ext "doc_core_cgl".
 #
-# Auto generated 04-10-2009 13:11
+# Auto generated 07-06-2010 21:35
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A description of the formal requirements or standards regarding PHP coding that you should adhere to when you develop TYPO3 Extensions or Core parts.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.3.2',
+	'version' => '4.4.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"3448";s:4:"TODO";s:4:"ec8a";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_cgl.pdf";s:4:"1ee6";s:14:"doc/manual.sxw";s:4:"8d6c";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"eaf4";s:4:"TODO";s:4:"ec8a";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_cgl.pdf";s:4:"2f42";s:20:"doc/doc_core_cgl.txt";s:4:"299f";s:14:"doc/manual.sxw";s:4:"8ab2";}',
 	'suggests' => array(
 	),
 );
