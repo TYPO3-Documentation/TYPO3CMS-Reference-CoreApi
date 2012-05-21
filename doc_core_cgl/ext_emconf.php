99348970f3fc7ebb84868de981e7ae357a27399a
Christopher <chrissitopher@gmx.de>
2012-05-21 22:20:20 +0200

    [TASK] Import doc_core_cgl 4.3
    Import doc_core_cgl for TYPO3 4.3.
    
    Change-Id: Ie37b746b00bab5f71c8faf2783b6f734610826c6

---
+++ doc_core_cgl/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "doc_core_cgl"
+#
+# Auto generated 04-10-2009 13:11
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 Coding Guidelines',
+	'description' => 'A description of the formal requirements or standards regarding PHP coding that you should adhere to when you develop TYPO3 Extensions or Core parts.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '4.3.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'TYPO3 Core Development Team',
+	'author_email' => 'info@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"3448";s:4:"TODO";s:4:"ec8a";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_cgl.pdf";s:4:"1ee6";s:14:"doc/manual.sxw";s:4:"8d6c";}',
+	'suggests' => array(
+	),
+);
+
+?>
