bc3ca813e96806fa1fdd8b563ca5c4021e466165
Christopher <chrissitopher@gmx.de>
2012-05-21 22:27:36 +0200

    [TASK] Import doc_core_cgl master
    Import doc_core_cgl, last version from SVN.
    
    Change-Id: I19eeb70ba2e71d8efc9a72a3f4403c0b512346c3

--- doc_core_cgl/ext_emconf.php
+++ doc_core_cgl/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_cgl".
 #
-# Auto generated 07-06-2010 21:35
+# Auto generated 11-04-2011 21:45
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,20 +15,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A description of the formal requirements or standards regarding PHP coding that you should adhere to when you develop TYPO3 Extensions or Core parts.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.4.1',
+	'version' => '4.5.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
+	'doNotLoadInFE' => 1,
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'TYPO3 Core Development Team',
-	'author_email' => 'info@typo3.org',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
@@ -40,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"eaf4";s:4:"TODO";s:4:"ec8a";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_cgl.pdf";s:4:"2f42";s:20:"doc/doc_core_cgl.txt";s:4:"299f";s:14:"doc/manual.sxw";s:4:"8ab2";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"4666";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_cgl.pdf";s:4:"974a";s:20:"doc/doc_core_cgl.txt";s:4:"9bb7";s:14:"doc/manual.sxw";s:4:"1b65";}',
 	'suggests' => array(
 	),
 );
