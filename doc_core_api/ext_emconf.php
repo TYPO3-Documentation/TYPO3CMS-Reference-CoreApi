355f77ec9f87c06d379b1eca3253ef310d8d66af
Christopher <chrissitopher@gmx.de>
2012-05-21 21:55:19 +0200

    [TASK] Import doc_core_api master
    Import doc_core_api, last version from SVN.
    
    Change-Id: I25a5d081307d16146a9c936fa9cc0cf669766c95

--- doc_core_api/ext_emconf.php
+++ doc_core_api/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_api".
 #
-# Auto generated 21-03-2010 20:23
+# Auto generated 11-11-2011 21:18
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,20 +15,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Reference to the Core APIs of TYPO3, e.g. $TCA, skinning API, Extension API, RTE API',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.3.0',
+	'version' => '4.3.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
+	'doNotLoadInFE' => 1,
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearcacheonload' => 0,
+	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'author' => 'TYPO3 Core Development Team',
-	'author_email' => 'info@typo3.org',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
@@ -40,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"984e";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_api.pdf";s:4:"2f49";s:20:"doc/doc_core_api.txt";s:4:"e5a2";s:14:"doc/manual.sxw";s:4:"2182";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"c3fb";s:12:"ext_icon.gif";s:4:"7277";s:20:"doc/doc_core_api.pdf";s:4:"b877";s:20:"doc/doc_core_api.txt";s:4:"13ab";s:14:"doc/manual.sxw";s:4:"3358";}',
 	'suggests' => array(
 	),
 );
