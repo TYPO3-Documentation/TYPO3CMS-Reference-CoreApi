d51b0b4913b5d3b155a424774deea9996a88ea69
Christopher <chrissitopher@gmx.de>
2012-05-22 17:00:19 +0200

    [TASK] Migrate doc_core_ts 4.7
    Import doc_core_ts for TYPO3 4.7, last version from SVN.
    
    Change-Id: I1fb04650e56012e2e94e627f0a166ebcf3ba1941

--- doc_core_ts/ext_emconf.php
+++ doc_core_ts/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "doc_core_ts".
 #
-# Auto generated 03-11-2011 17:01
+# Auto generated 22-04-2012 20:23
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -12,10 +12,10 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TypoScript Syntax and In-depth Study',
-	'description' => '',
+	'description' => 'Description of the syntax and nature of TypoScript.',
 	'category' => 'doc',
 	'shy' => 0,
-	'version' => '4.6.0',
+	'version' => '4.7.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"d7e3";s:12:"ext_icon.gif";s:4:"7277";s:19:"doc/doc_core_ts.pdf";s:4:"52de";s:19:"doc/doc_core_ts.txt";s:4:"51f7";s:14:"doc/manual.sxw";s:4:"eb44";}',
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"eba1";s:12:"ext_icon.gif";s:4:"7277";s:19:"doc/doc_core_ts.pdf";s:4:"cc6b";s:19:"doc/doc_core_ts.txt";s:4:"33f4";s:14:"doc/manual.sxw";s:4:"acc4";}',
 	'suggests' => array(
 	),
 );
