1fb5fbd52ffc0d57ad48c9b075bf6f0b6aa30489
Christopher <chrissitopher@gmx.de>
2012-05-22 15:44:41 +0200

    [TASK] Migrate doc_core_services master
    Import doc_core_services, last version from SVN.
    
    Change-Id: I562ae193a7d73f50ba9b82f88bc9514db3ea6023

---
+++ doc_core_services/ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "doc_core_services"
+#
+# Auto generated 09-03-2009 09:59
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 Services',
+	'description' => 'Describes the usage and API of services available in TYPO3',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '1.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'doNotLoadInFE' => 1,
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"8950";s:12:"ext_icon.gif";s:4:"7277";s:25:"doc/doc_core_services.pdf";s:4:"2f41";s:25:"doc/doc_core_services.txt";s:4:"e76e";s:14:"doc/manual.sxw";s:4:"6771";}',
+	'suggests' => array(
+	),
+);
+
+?>
