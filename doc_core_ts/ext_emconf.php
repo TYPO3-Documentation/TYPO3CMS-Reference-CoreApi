9b589846a0caed9bf6b1e5eb7aa1f944e8d69f25
Christopher <chrissitopher@gmx.de>
2012-05-22 16:52:35 +0200

    [TASK] Migrate doc_core_ts 4.2
    Import doc_core_ts for TYPO3 4.2.
    
    Change-Id: Ib1664c0d4f856b9f6fbd10bd910ab14254950fbd

---
+++ doc_core_ts/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "doc_core_ts"
+#
+# Auto generated 01-12-2008 01:52
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TypoScript Syntax and In-depth Study',
+	'description' => '',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '4.2.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'TYPO3 Core Development Team',
+	'author_email' => 'info@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:3:{s:9:"ChangeLog";s:4:"4bb8";s:12:"ext_icon.gif";s:4:"7277";s:14:"doc/manual.sxw";s:4:"b30e";}',
+	'suggests' => array(
+	),
+);
+
+?>
