ea1558e6c2801c74b7a0f26d1632d6edf455ff26
Christopher <chrissitopher@gmx.de>
2012-05-23 15:15:57 +0200

    [TASK] Migrate doc_guide_security 1.0.0
    Import doc_guide_security 1.0.0.
    
    Change-Id: If0fc55665e97af33464d353cdd9e911c56a31c5b

---
+++ doc_guide_security/ext_emconf.php
@@ -0,0 +1,49 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "doc_guide_security".
+#
+# Auto generated 05-12-2011 20:00
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Security Guide',
+	'description' => 'This document describes some typical risks and advises how to protect a TYPO3 site in order to ensure it is and stays secure and stable.',
+	'category' => 'doc',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'doNotLoadInFE' => 1,
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Official Documentation',
+	'author_email' => 'documentation@typo3.org',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"2576";s:12:"ext_icon.gif";s:4:"7277";s:28:"doc/TYPO3_Security_Guide.pdf";s:4:"0126";s:14:"doc/manual.sxw";s:4:"9c22";s:14:"doc/manual.txt";s:4:"6442";s:38:"res/images/be-user-expiry-date-004.png";s:4:"ec34";s:47:"res/images/change-install-tool-password-001.png";s:4:"4202";s:37:"res/images/directory-indexing-001.png";s:4:"ba20";s:43:"res/images/editor-admin-privileges-002a.png";s:4:"66b1";s:43:"res/images/editor-admin-privileges-002b.png";s:4:"1d45";s:42:"res/images/editor-change-password-003a.png";s:4:"0cc1";s:42:"res/images/editor-change-password-003b.png";s:4:"69e2";s:42:"res/images/editor-notify-on-login-001a.png";s:4:"fb31";s:42:"res/images/editor-notify-on-login-001b.png";s:4:"1717";s:38:"res/images/enable-install-tool-001.png";s:4:"5885";s:34:"res/images/extension-state-001.png";s:4:"a748";}',
+	'suggests' => array(
+	),
+);
+
+?>
