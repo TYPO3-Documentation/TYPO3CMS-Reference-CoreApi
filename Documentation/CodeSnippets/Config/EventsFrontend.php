f141755481db20ca7ced8bf78c13186f0c43fb34
lina.wolf <lwolf@w-commerce.de>
2022-07-03 18:00:37 +0200

    [TASK] Move code-snippet creation to new tool (v 11.5)
    Delete ModifyNewContentElementWizardItemsEvent.rst.txt that does not exist in 11.5 and was backported by error

---
+++ Documentation/CodeSnippets/Config/EventsFrontend.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        "action"=> "createPhpClassDocs",
+        "class"=> \TYPO3\CMS\Frontend\Event\ModifyHrefLangTagsEvent::class,
+        "targetFileName"=> "Events/Frontend/ModifyHrefLangTagsEvent.rst.txt",
+        "withCode"=> false
+    ],
+    [
+        "action"=> "createPhpClassDocs",
+        "class"=> \TYPO3\CMS\Frontend\Authentication\ModifyResolvedFrontendGroupsEvent::class,
+        "targetFileName"=> "Events/Frontend/ModifyResolvedFrontendGroupsEvent.rst.txt",
+        "withCode"=> false
+    ],
+];
