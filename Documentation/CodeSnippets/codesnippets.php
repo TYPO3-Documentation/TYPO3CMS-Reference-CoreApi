63d4b1c3a942a252abe7d05e34266352f376006e
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-07-11 17:11:15 +0200

    [TASK] Move code-snippet creation to new tool (v 11.5) (#1932)
    Delete ModifyNewContentElementWizardItemsEvent.rst.txt that does not exist in 11.5 and was backported by error
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>

---
+++ Documentation/CodeSnippets/codesnippets.php
@@ -0,0 +1,22 @@
+<?php
+
+    // https://github.com/TYPO3-Documentation/t3docs-codesnippets
+    // ddev exec vendor/bin/typo3  restructured_api_tools:php_domain public/fileadmin/TYPO3CMS-Reference-CoreApi/Documentation/CodeSnippets/
+
+    return array_merge(
+        include('Config/EventsBackend.php'),
+        include('Config/EventsCore.php'),
+        include('Config/EventsCoreResource.php'),
+        include('Config/EventsExtbase.php'),
+        include('Config/EventsExtensionManager.php'),
+        include('Config/EventsFilelist.php'),
+        include('Config/EventsFrontend.php'),
+        include('Config/EventsFrontendLogin.php'),
+        include('Config/EventsImpexp.php'),
+        include('Config/EventsInstall.php'),
+        include('Config/EventsLinkvalidator.php'),
+        include('Config/EventsRecordlist.php'),
+        include('Config/EventsSeo.php'),
+        include('Config/EventsSetup.php'),
+        include('Config/EventsWorkspaces.php'),
+    );
