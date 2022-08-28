51782e8fda4bd11bf2b0dcfa580d9c4e5c3d545b
lina.wolf <lwolf@w-commerce.de>
2022-08-28 09:51:08 +0200

    Start a tea tutorial

---
+++ Documentation/CodeSnippets/Config/Tutorials/Tea.php
@@ -0,0 +1,14 @@
+<?php
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+return [
+    /*
+    [
+        'action'=> 'createCodeSnippet',
+        'caption' => 'EXT:tea/composer.json',
+        'sourceFile'=> 'typo3conf/ext/tea/composer.json',
+        'targetFileName' => 'Extbase/Tutorials/Tea/ComposerJson.rst.txt'
+    ],
+    */
+];
