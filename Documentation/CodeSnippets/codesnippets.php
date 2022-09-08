1c7793833ebe5e27a1bbe4f21c336264a8211127
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-09-08 09:21:30 +0200

    Generate Codesnippets directly from the repository (#2176)
    * Generate Codesnippets directly from the repository
    
    No more special project is needed
    
    * Generate Codesnippets directly from the repository
    
    remove typo3-minimal
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>

--- Documentation/CodeSnippets/codesnippets.php
+++ Documentation/CodeSnippets/codesnippets.php
@@ -1,7 +1,7 @@
 <?php
 
     // https://github.com/TYPO3-Documentation/t3docs-codesnippets
-    // ddev exec vendor/bin/typo3  restructured_api_tools:php_domain public/fileadmin/TYPO3CMS-Reference-CoreApi/Documentation/CodeSnippets/
+    // ddev exec .Build/vendor/bin/typo3 codesnippet:create Documentation/CodeSnippets/
 
     return array_merge(
         include('Config/Extbase.php'),
