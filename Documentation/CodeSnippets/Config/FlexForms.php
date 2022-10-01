ec0a273d55d2ac6cbeb1d67bea68c9a93fc198fa
lina.wolf <lwolf@w-commerce.de>
2022-10-01 10:19:16 +0000

    emphasizeLines in codesnippet

--- Documentation/CodeSnippets/Config/FlexForms.php
+++ Documentation/CodeSnippets/Config/FlexForms.php
@@ -7,18 +7,19 @@ return [
         'action'=> 'createCodeSnippet',
         'caption' => 'EXT:felogin/Configuration/FlexForms/Login.xml',
         'sourceFile'=> 'typo3/sysext/felogin/Configuration/FlexForms/Login.xml',
-        'targetFileName' => 'FlexForms/FeLogin.rst.txt'
+        'targetFileName' => 'FlexForms/FeLogin.rst.txt',
     ],
     [
         'action'=> 'createCodeSnippet',
         'caption' => 'EXT:examples/Configuration/Flexforms/PluginHaikuList.xml',
         'sourceFile'=> 'EXT:examples/Configuration/Flexforms/PluginHaikuList.xml',
-        'targetFileName' => 'FlexForms/Examples/PluginHaikuList.rst.txt'
+        'targetFileName' => 'FlexForms/Examples/PluginHaikuList.rst.txt',
     ],
     [
         'action'=> 'createCodeSnippet',
+        'emphasizeLines' => ['19', '20', '21', '22'],
         'caption' => 'EXT:examples/Configuration/TCA/Overrides/tt_content_plugin_haiku_list.php',
         'sourceFile'=> 'EXT:examples/Configuration/TCA/Overrides/tt_content_plugin_haiku_list.php',
-        'targetFileName' => 'FlexForms/Examples/PluginHaikuListRegistration.rst.txt'
+        'targetFileName' => 'FlexForms/Examples/PluginHaikuListRegistration.rst.txt',
     ],
 ];
