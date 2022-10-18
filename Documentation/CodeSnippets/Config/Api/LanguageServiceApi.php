75b27704037f0d028727e442839f8c86d029acb7
lina.wolf <lwolf@w-commerce.de>
2022-10-18 09:28:11 +0000

    Plain PHP Localization

---
+++ Documentation/CodeSnippets/Config/Api/LanguageServiceApi.php
@@ -0,0 +1,24 @@
+<?php
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+return [
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Extbase\Utility\LocalizationUtility::class,
+        'targetFileName' => 'CodeSnippets/ApiOverview/Internationalization/LocalizationApi/_phpdocs/LocalizationUtilityApi.rst.txt',
+        'withCode' => false
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Extbase\Utility\LocalizationUtility::class,
+        'targetFileName' => 'CodeSnippets/Extbase/LocalizationUtilityApi.rst.txt',
+        'withCode' => false
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Extbase\Utility\LocalizationUtility::class,
+        'targetFileName' => 'CodeSnippets/Extbase/LocalizationUtilityApi.rst.txt',
+        'withCode' => false
+    ],
+];
