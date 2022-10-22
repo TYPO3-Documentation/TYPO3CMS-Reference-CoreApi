b393f643915757026d5b208986a64ef567e13214
lina.wolf <lwolf@w-commerce.de>
2022-10-22 14:01:47 +0000

    [TASK] Replace API Descriptions in Session Management
    resolves #2291

---
+++ Documentation/CodeSnippets/Config/Api/SessionManagement.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Session\SessionManager::class,
+        'targetFileName' => 'ApiOverview/SessionStorageFramework/_SessionManager.rst.txt',
+        'withCode' => false
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Session\UserSession::class,
+        'targetFileName' => 'ApiOverview/SessionStorageFramework/_UserSession.rst.txt',
+        'withCode' => false
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Session\UserSessionManager::class,
+        'targetFileName' => 'ApiOverview/SessionStorageFramework/_UserSessionManager.rst.txt',
+        'withCode' => false
+    ],
+];
