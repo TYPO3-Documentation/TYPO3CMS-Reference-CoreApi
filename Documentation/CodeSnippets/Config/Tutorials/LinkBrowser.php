062ddb14e5d4415e5d359cada3f7f79827cb55dd
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2022-11-04 08:17:38 +0100

    [Feature] Create a custom link browser tutorial (#2385)
    * [Feature] Create a custom link browser tutorial
    
    Next steps: Overhaul an unify the api section of the link handler. Link from and to link handler API once that section is cleared up.
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/LinkBrowser/Tutorials/CustomLinkBrowser.rst
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Regenerate Codesnippets
    
    * Document page tsconfig and di
    
    * Update Documentation/ApiOverview/LinkBrowser/Tutorials/CustomLinkBrowser.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/LinkBrowser/Tutorials/CustomLinkBrowser.rst
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/LinkBrowser/Tutorials/CustomLinkBrowser.rst
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    (cherry picked from commit 6d516826d7c29edf30d3e1d6c827af0c37b37bbb)

--- Documentation/CodeSnippets/Config/Tutorials/LinkBrowser.php
+++ Documentation/CodeSnippets/Config/Tutorials/LinkBrowser.php
@@ -14,4 +14,79 @@ return [
         'sourceFile'=> 'EXT:examples/Configuration/TypoScript/RecordLinks/Haiku.typoscript',
         'targetFileName'=> 'Tutorials/LinkBrowser/Classes/HaikuRecordLinkTypoScript.rst.txt',
     ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GitHubLinkHandler::class,
+        'members' => [
+            'linkAttributes',
+            'linkParts',
+            'view',
+            'configuration',
+            '__construct',
+            'initialize',
+            'setView',
+        ],
+        'targetFileName' => 'ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GitHubLinkHandlerInitialize.rst.txt',
+    ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GitHubLinkHandler::class,
+        'members' => [
+            'canHandleLink',
+        ],
+        'targetFileName' => 'ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GitHubLinkHandlerCanHandleLink.rst.txt',
+    ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GitHubLinkHandler::class,
+        'members' => [
+            'formatCurrentUrl',
+        ],
+        'targetFileName' => 'ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GitHubLinkHandlerFormatCurrentUrl.rst.txt',
+    ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GitHubLinkHandler::class,
+        'members' => [
+            'render',
+        ],
+        'targetFileName' => '/ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GitHubLinkHandlerRender.rst.txt',
+    ],
+    [
+        'action' => 'createCodeSnippet',
+        'caption' => 'EXT:examples/Resources/Public/JavaScript/github_link_handler.js',
+        'sourceFile' => 'EXT:examples/Resources/Public/JavaScript/github_link_handler.js',
+        'language' => 'js',
+        'targetFileName' => '/ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_CustomLinkHandlerJavaScript.rst.txt',
+    ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GitHubLinkHandling::class,
+        'members' => [
+            'baseUrn',
+            'asString',
+            'resolveHandlerData',
+        ],
+        'withComment' => false,
+        'withClassComment' => true,
+        'targetFileName' => '/ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GitHubLinkHandling.rst.txt',
+    ],
+    [
+        'action' => 'createPhpClassCodeSnippet',
+        'class' => \T3docs\Examples\LinkHandler\GithubLinkBuilder::class,
+        'members' => [
+            'TYPE_GITHUB',
+            'build',
+        ],
+        'withComment' => false,
+        'withClassComment' => true,
+        'targetFileName' => '/ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_GithubLinkBuilder.rst.txt',
+    ],
+    [
+        'action' => 'createCodeSnippet',
+        'caption' => 'EXT:examples/Configuration/TsConfig/Page/LinkBrowser/GitHubLinkhandler.tsconfig',
+        'sourceFile' => 'EXT:examples/Configuration/TsConfig/Page/LinkBrowser/GitHubLinkhandler.tsconfig',
+        'language' => 'typoscript',
+        'targetFileName' => 'ApiOverview/LinkBrowser/Tutorials/_CustomLinkBrowser/_PageTsConfig.rst.txt',
+    ],
 ];
