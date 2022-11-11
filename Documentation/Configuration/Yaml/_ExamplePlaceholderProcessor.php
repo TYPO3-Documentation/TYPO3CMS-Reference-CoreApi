a6e7a3b3762835a55b11fbc5a60eea3b07b549e9
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-11-11 08:43:18 +0100

    [TASK] Revise chapter "YAML API" (#2438) (#2451)
    Releases: main, 11.5

---
+++ Documentation/Configuration/Yaml/_ExamplePlaceholderProcessor.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Configuration\Processor\Placeholder;
+
+use TYPO3\CMS\Core\Configuration\Processor\Placeholder\PlaceholderProcessorInterface;
+
+final class ExamplePlaceholderProcessor implements PlaceholderProcessorInterface
+{
+    public function canProcess(string $placeholder, array $referenceArray): bool
+    {
+        return strpos($placeholder, '%example(') !== false;
+    }
+
+    public function process(string $value, array $referenceArray)
+    {
+        // do some processing
+        $result = $this->getValue($value);
+
+        // Throw this exception if the placeholder can't be substituted
+        if ($result === null) {
+            throw new \UnexpectedValueException('Value not found', 1581596096);
+        }
+        return $result;
+    }
+
+    private function getValue(string $value): ?string
+    {
+        // implement logic to fetch specific values from an external service
+        // or just add simple mapping logic - whatever is appropriate
+        $aliases = [
+            'foo' => 'F-O-O',
+            'bar' => 'ARRRRR',
+        ];
+        return $aliases[$value] ?? null;
+    }
+}
