c5da8dce5dd660d5b609e61362910a8df86a2b21
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-11-27 19:19:15 +0100

    [TASK] Revise Database/ConnectionPool chapter
    Mainly, get rid of the GeneralUtility::makeInstance() usage.
    Also streamline wordings.
    
    Releases: main, 11.5

---
+++ Documentation/ApiOverview/Database/ConnectionPool/_MyTableRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vendor\MyExtension\Domain\Repository;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
+final class MyTableRepository
+{
+    private const TABLE_NAME = 'tx_myextension_domain_model_mytable';
+
+    private ConnectionPool $connectionPool;
+
+    public function __construct(ConnectionPool $connectionPool)
+    {
+        $this->connectionPool = $connectionPool;
+    }
+
+    public function findSomething()
+    {
+        // Get a query builder for a table
+        $queryBuilder = $this->connectionPool
+            ->getQueryBuilderForTable(self::TABLE_NAME);
+
+        // Or get a connection for a table
+        $connection = $this->connectionPool
+            ->getConnectionForTable(self::TABLE_NAME);
+    }
+}
