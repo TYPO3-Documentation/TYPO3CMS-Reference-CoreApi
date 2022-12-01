ef47dbf66aad07d51ea8c266179e6de96b301fa3
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-12-01 14:14:38 +0100

    [TASK] Use constructor property promotion in database CRUD examples
    Releases: main

--- Documentation/ApiOverview/Database/BasicCrud/_MyQueryBuilderSelectRepository.php
+++ Documentation/ApiOverview/Database/BasicCrud/_MyQueryBuilderSelectRepository.php
@@ -11,11 +11,9 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class MyQueryBuilderSelectRepository
 {
-    private ConnectionPool $connectionPool;
-
-    public function __construct(ConnectionPool $connectionPool)
-    {
-        $this->connectionPool = $connectionPool;
+    public function __construct(
+        private readonly ConnectionPool $connectionPool
+    ) {
     }
 
     public function selectSomeData(): array
