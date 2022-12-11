83e64698bcbc035482bd3b0a9a52c22c4aa99715
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-12-11 09:27:47 +0100

    [Backport 11.5] [TASK] Rework assets chapter (#2524) (#2544)
    Mainly:
    - Use dependency injection in examples
    - Remove list of former methods (TSFE, page renderer) to the end ($GLOBALS['TSFE']->setJS() does not exist anymore in v11)
    - Add TOC
    - Adjust wording
    
    Resolves: https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-CoreApi/issues/1934
    Releases: main, 11.5
    
    Co-authored-by: Lina Wolf <48202465+linawolf@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Assets/_MyClassWithAssetCollector.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\MyClass;
+
+use TYPO3\CMS\Core\Page\AssetCollector;
+
+final class MyClass
+{
+    private AssetCollector $assetCollector;
+
+    public function __construct(AssetCollector $assetCollector)
+    {
+        $this->assetCollector = $assetCollector;
+    }
+
+    public function doSomething()
+    {
+        // $this->assetCollector can now be used
+        // see examples below
+    }
+}
