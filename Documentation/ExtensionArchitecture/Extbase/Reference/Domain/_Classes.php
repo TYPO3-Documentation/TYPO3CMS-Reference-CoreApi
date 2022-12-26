43eef3b783333245a082e2d43c55af0542408614
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-12-26 15:13:06 +0100

    [FEATURE] Create a custom model for a Core table
    Releases: main, 11.5

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/_Classes.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    \MyVendor\MyExtension\Domain\Model\Content::class => [
+        'tableName' => 'tt_content',
+    ],
+];
---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/_ContentRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class ContentRepository extends Repository
+{
+}
