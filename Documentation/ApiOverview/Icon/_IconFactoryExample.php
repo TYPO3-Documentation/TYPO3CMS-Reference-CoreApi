9cb895f3aa17e75f1be9dbbdebc384f598686eb6
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-01-15 18:35:25 +0100

    [TASK] Streamline icon API chapter
    Mainly: Use constructor injection in PHP example
    
    Releases: main, 11.5

---
+++ Documentation/ApiOverview/Icon/_IconFactoryExample.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension;
+
+use TYPO3\CMS\Core\Imaging\Icon;
+
+final class MyClass
+{
+    public function __construct(
+        private readonly IconFactory $iconFactory
+    ) {
+    }
+
+    public function doSomething()
+    {
+        $icon = $this->iconFactory->getIcon(
+            'tx-myext-action-preview',
+            Icon::SIZE_SMALL,
+            'overlay-identifier'
+        );
+
+        // Do something with the icon, for example, assign it to the view
+        // $this->view->assign('icon', $icon);
+    }
+}
