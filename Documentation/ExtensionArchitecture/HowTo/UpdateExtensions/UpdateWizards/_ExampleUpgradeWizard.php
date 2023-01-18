cc92e3443a99d6b7906be0c76f74acd7c02e4528
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-01-18 19:48:14 +0100

    [TASK] Streamline upgrade wizards section
    Mainly:
    - Use "upgrade" instead of "update"
    - Extract wizard example class into separate file
    - Substitute ChattyInterface example class
    - Link directly to interface source code
    
    Releases: main, 11.5

---
+++ Documentation/ExtensionArchitecture/HowTo/UpdateExtensions/UpdateWizards/_ExampleUpgradeWizard.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Upgrades;
+
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+final class ExampleUpgradeWizard implements UpgradeWizardInterface
+{
+    /**
+     * Return the identifier for this wizard
+     * This should be the same string as used in the ext_localconf.php class registration
+     */
+    public function getIdentifier(): string
+    {
+        return 'myExtension_exampleUpgradeWizard';
+    }
+
+    /**
+     * Return the speaking name of this wizard
+     */
+    public function getTitle(): string
+    {
+        return 'Title of this updater';
+    }
+
+    /**
+     * Return the description for this wizard
+     */
+    public function getDescription(): string
+    {
+        return 'Description of this updater';
+    }
+
+    /**
+     * Execute the update
+     *
+     * Called when a wizard reports that an update is necessary
+     */
+    public function executeUpdate(): bool
+    {
+        // Add your logic here
+    }
+
+    /**
+     * Is an update necessary?
+     *
+     * Is used to determine whether a wizard needs to be run.
+     * Check if data for migration exists.
+     *
+     * @return bool Whether an update is required (TRUE) or not (FALSE)
+     */
+    public function updateNecessary(): bool
+    {
+        // Add your logic here
+    }
+
+    /**
+     * Returns an array of class names of prerequisite classes
+     *
+     * This way a wizard can define dependencies like "database up-to-date" or
+     * "reference index updated"
+     *
+     * @return string[]
+     */
+    public function getPrerequisites(): array
+    {
+        // Add your logic here
+    }
+}
