72e2071f222befa8ee674f3ef1c6dfa433e0672f
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-01-18 18:00:00 +0100

    [Backport 11.5] [TASK] Streamline backend routing chapter (#2606) (#2612)
    Mainly:
    - Reduce Routes.php example to the relevant part (remove large comment)
    - Use constructor injection in UriBuilder example
    - Link to TYPO3 request object chapter instead external PSR-7 page (this should be more helpful)
    
    Releases: main, 11.5

---
+++ Documentation/ApiOverview/Backend/_BackendRouting/_UriBuilderExample.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Routing\UriBuilder;
+
+final class MyRouteController
+{
+    private UriBuilder $uriBuilder;
+
+    public function __construct(UriBuilder $uriBuilder)
+    {
+        $this->uriBuilder = $uriBuilder;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        // .. do some stuff
+
+        // Using a route identifier
+        $uri = $this->uriBuilder->buildUriFromRoute(
+            'web_layout',
+            ['id' => 42]
+        );
+
+        // Using a route path
+        $uri = $this->uriBuilder->buildUriFromRoutePath(
+            '/record/edit',
+            [
+                'edit' => [
+                    'pages' => [
+                        123 => 'edit',
+                    ],
+                ],
+            ]
+        );
+
+        // ... do some other stuff
+    }
+}
