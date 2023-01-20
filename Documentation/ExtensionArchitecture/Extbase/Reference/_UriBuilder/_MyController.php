064bb56ee50bd8ba21cae13668fc4df92e1aa12b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-01-20 19:24:27 +0100

    [FEATURE] Add Extbase URI builder chapter
    Releases: main, 11.5

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_UriBuilder/_MyController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller\MyController;
+
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+final class MyController extends ActionController
+{
+    public function myAction(): ResponseInterface
+    {
+        $url = $this->uriBuilder
+            ->reset()
+            ->setTargetPageUid(42)
+            ->uriFor(
+                'anotherAction',
+                [
+                    'myRecord' => 21,
+                ],
+                'MyController',
+                'myextension',
+                'myplugin'
+            );
+
+        // do something with $url
+    }
+}
