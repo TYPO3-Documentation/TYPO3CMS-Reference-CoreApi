d9dadaad5eafb7a622f27bf13cdaf424c2f464ce
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-01-21 06:28:24 +0100

    [FEATURE] #98394 - Introduce event to prevent downloading of language packs (#2622)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/332
    Releases: main

--- Documentation/CodeSnippets/Config/Api/Events/EventsInstall.php
+++ Documentation/CodeSnippets/Config/Api/Events/EventsInstall.php
@@ -7,4 +7,10 @@ return [
         'targetFileName' => 'CodeSnippets/Events/Install/ModifyLanguagePackRemoteBaseUrlEvent.rst.txt',
         'withCode' => false,
     ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Install\Service\Event\ModifyLanguagePacksEvent::class,
+        'targetFileName' => 'CodeSnippets/Events/Install/ModifyLanguagePacksEvent.rst.txt',
+        'withCode' => false,
+    ],
 ];
