e6055a924a0803c6318065efa16561397de95c58
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-02-12 06:03:00 +0100

    [TASK] Move file list events code snippets into separate files (#2679)
    This is the first PR in a series to move the code snippets into separate files. This way,
    we can adapt coding styles easily and migrate PHP code to newer PHP versions via Rector
    automatically.
    
    - YAML configuration is moved into separate files
    - PHP classes are moved into separate files
    - Namespaces are adjusted (from EventListener -> FileList)
    - Extension key is moved from "my_package" to "my_extension" to be more generic
      (and to avoid similarity with site package)
    
    When this PR is merged, the other events are adjusted in the same manner.
    
    Releases: main, 11.5

---
+++ Documentation/ApiOverview/Events/Events/Filelist/_Snippets/_ModifyEditFileFormDataEventListener.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\FileList;
+
+use TYPO3\CMS\Filelist\Event\ModifyEditFileFormDataEvent;
+
+final class ModifyEditFileFormDataEventListener
+{
+    public function __invoke(ModifyEditFileFormDataEvent $event): void
+    {
+        // Get current form data
+        $formData = $event->getFormData();
+
+        // Change TCA "renderType" based on the file extension
+        $fileExtension = $event->getFile()->getExtension();
+        if ($fileExtension === 'ts') {
+            $formData['processedTca']['columns']['data']['config']['renderType'] = 'tsRenderer';
+        }
+
+        // Set updated form data
+        $event->setFormData($formData);
+    }
+}
