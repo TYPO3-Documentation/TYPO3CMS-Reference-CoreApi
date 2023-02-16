d1b0cf449abc4307aebbdacbff800329d69e6aeb
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-02-16 19:27:35 +0100

    [TASK] Streamline "Site handling" > "Site settings" chapter
    Mainly:
    - Move YAML/PHP snippets into separate files
    - Add line length for better readability
    
    Releases: main, 11.5

---
+++ Documentation/ApiOverview/SiteHandling/_extending-site-config.php
@@ -0,0 +1,19 @@
+<?php
+
+// Experimental example to add a new field to the site configuration
+
+// Configure a new simple required input field to site
+$GLOBALS['SiteConfiguration']['site']['columns']['myNewField'] = [
+    'label' => 'A new custom field',
+    'config' => [
+        'type' => 'input',
+        'eval' => 'required',
+    ],
+];
+
+// And add it to showitem
+$GLOBALS['SiteConfiguration']['site']['types']['0']['showitem'] = str_replace(
+    'base,',
+    'base, myNewField, ',
+    $GLOBALS['SiteConfiguration']['site']['types']['0']['showitem']
+);
