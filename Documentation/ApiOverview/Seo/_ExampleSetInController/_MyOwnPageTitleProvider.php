40b04882a1741a11d2ba297b11d656be4d0d137b
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-02-27 18:45:08 +0100

    Update Documentation/ApiOverview/Seo/_ExampleSetInController/_MyOwnPageTitleProvider.php

--- Documentation/ApiOverview/Seo/_ExampleSetInController/_MyOwnPageTitleProvider.php
+++ Documentation/ApiOverview/Seo/_ExampleSetInController/_MyOwnPageTitleProvider.php
@@ -8,7 +8,7 @@ use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
 
 final class MyOwnPageTitleProvider extends AbstractPageTitleProvider
 {
-    public function setTitle(string $title)
+    public function setTitle(string $title): void
     {
         $this->title = $title;
     }
