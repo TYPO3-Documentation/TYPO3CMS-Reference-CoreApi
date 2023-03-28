200631752cdb812ccb3ccaf566f9ab979b9ebcdd
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-03-28 10:45:18 +0200

    [FEATURE] #99803 - Introduce new PSR-14 BeforeRedirectMatchDomainEvent (#2793)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/400
    Releases: main
    
    ---------
    
    Co-authored-by: Lina Wolf <48202465+linawolf@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Events/Events/Redirects/_BeforeRedirectMatchDomainEvent/_MyEventListener.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Redirects\EventListener;
+
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Redirects\Event\BeforeRedirectMatchDomainEvent;
+
+final class MyEventListener
+{
+    public function __invoke(BeforeRedirectMatchDomainEvent $event): void
+    {
+        $matchedRedirectRecord = $this->customRedirectMatching($event);
+        if ($matchedRedirectRecord !== null) {
+            $event->setMatchedRedirect($matchedRedirectRecord);
+        }
+    }
+
+    private function customRedirectMatching(BeforeRedirectMatchDomainEvent $event): ?array
+    {
+        // @todo Implement custom redirect record loading and matching. If
+        //       a redirect based on custom logic is determined, return the
+        //       :sql:`sys_redirect` tables conform redirect record.
+
+        // Note: Below is simplified example code with no real value.
+        $record = BackendUtility::getRecord('sys_redirect', 123);
+
+        // Do custom matching logic against the record and return matched
+        // record - if there is one.
+        if ($record /* && custom condition against the record */) {
+            return $record;
+        }
+
+        // Return null to indicate that no matched redirect could be found
+        return null;
+    }
+}
