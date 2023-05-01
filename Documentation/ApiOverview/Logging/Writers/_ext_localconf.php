b4b3ab6d458a02e81d227391edcdf81d59998786
lina.wolf <lwolf@w-commerce.de>
2023-05-01 11:09:26 +0000

    Unify the ext_localconf.php for Logging Writers
    references #2782

---
+++ Documentation/ApiOverview/Logging/Writers/_ext_localconf.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Log\LogLevel;
+use TYPO3\CMS\Core\Log\Writer\DatabaseWriter;
+
+defined('TYPO3') or die();
+
+
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['T3docs']['Examples']['Controller']['writerConfiguration'] = [
+    LogLevel::DEBUG => [
+        DatabaseWriter::class => [
+            'logTable' => 'tx_examples_log',
+        ],
+    ],
+];
