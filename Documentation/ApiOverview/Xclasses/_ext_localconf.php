f610de7bd4042860698c5b0c80e8ad079f534f13
lina.wolf <lwolf@w-commerce.de>
2023-05-01 12:05:49 +0000

    Unify the ext_localconf.php for XClasses
    references #2782

---
+++ Documentation/ApiOverview/Xclasses/_ext_localconf.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use MyVendor\MyExtension\Xclass\NewRecordController as NewRecordControllerXclass;
+use TYPO3\CMS\Backend\Controller\NewRecordController;
+
+defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][NewRecordController::class] = [
+    'className' => NewRecordControllerXclass::class
+];
