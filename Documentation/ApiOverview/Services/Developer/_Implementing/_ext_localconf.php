123579eafb55a26f5625b98f01b0d1428fc9a9b6
lina.wolf <lwolf@w-commerce.de>
2023-05-01 11:55:20 +0000

    Unify the ext_localconf.php for Openauth Service
    references #2782

---
+++ Documentation/ApiOverview/Services/Developer/_Implementing/_ext_localconf.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Foo\Babelfish\Service\Translator;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die();
+
+ExtensionManagementUtility::addService(
+// Extension Key
+    'babelfish',
+    // Service type
+    'translator',
+    // Service key
+    'tx_babelfish_translator',
+    [
+        'title' => 'Babelfish',
+        'description' => 'Guess alien languages by using a babelfish',
+
+        'subtype' => '',
+
+        'available' => true,
+        'priority' => 60,
+        'quality' => 80,
+
+        'os' => '',
+        'exec' => '',
+
+        'className' => Translator::class,
+    ]
+);
