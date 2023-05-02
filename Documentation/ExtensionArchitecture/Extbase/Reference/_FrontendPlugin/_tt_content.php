9c0319b7e4fc7d71ec4b85ceeee18aa27458f07a
lina.wolf <lwolf@w-commerce.de>
2023-05-02 04:12:07 +0000

    Apply changes from code review

--- Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_tt_content.php
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_tt_content.php
@@ -5,7 +5,7 @@ defined('TYPO3') or die();
 (static function (): void {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
         // extension name, matching the PHP namespaces (but without the vendor)
-        'blog_example',
+        'BlogExample',
         // arbitrary, but unique plugin name (not visible in the backend)
         'PostSingle',
         // plugin title, as visible in the drop-down in the backend, use "LLL:" for localization
