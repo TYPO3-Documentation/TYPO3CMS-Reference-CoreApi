37ab4e722a0811a12ed31042e4cd88031a7cd38f
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2023-05-03 11:09:05 +0200

    Update Documentation/ApiOverview/Events/Hooks/_SomeClassPostProc.php
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

--- Documentation/ApiOverview/Events/Hooks/_SomeClassPostProc.php
+++ Documentation/ApiOverview/Events/Hooks/_SomeClassPostProc.php
@@ -4,7 +4,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class SomeClass
 {
-    public function doSomeThing()
+    public function doSomeThing(): void
     {
         // Call post-processing function for constructor:
         if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][static::class]['Some-PostProc'])) {
