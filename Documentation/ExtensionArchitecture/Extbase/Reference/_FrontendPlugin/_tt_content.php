d5cb7db744779d7f21d90b3967f4c6c771cbbb30
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2023-05-03 11:07:26 +0200

    Overhaul the Extbase Frontend Plugin section (#2902)
    * Overhaul the Extbase Frontend Plugin section
    
    Also unify the ext_localconf structure
    
    references #2782
    
    * Update Documentation/ExtensionArchitecture/Extbase/Reference/FrontendPlugins.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ExtensionArchitecture/Extbase/Reference/FrontendPlugins.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ExtensionArchitecture/Extbase/Reference/FrontendPlugins.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_ext_localconf_rss.php
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Apply changes from code review
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_tt_content.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('TYPO3') or die();
+
+(static function (): void {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+        // extension name, matching the PHP namespaces (but without the vendor)
+        'BlogExample',
+        // arbitrary, but unique plugin name (not visible in the backend)
+        'PostSingle',
+        // plugin title, as visible in the drop-down in the backend, use "LLL:" for localization
+        'Single Post (BlogExample)'
+    );
+})();
