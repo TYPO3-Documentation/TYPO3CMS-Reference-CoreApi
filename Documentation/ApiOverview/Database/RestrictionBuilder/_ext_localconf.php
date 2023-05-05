053f424400f56d7fb95f4839e22e96055133d245
lina.wolf <lwolf@w-commerce.de>
2023-05-05 14:44:07 +0000

    Apply changes from code review

--- Documentation/ApiOverview/Database/RestrictionBuilder/_ext_localconf.php
+++ Documentation/ApiOverview/Database/RestrictionBuilder/_ext_localconf.php
@@ -6,6 +6,4 @@ use MyVendor\MyExtension\Database\Query\Restriction\CustomRestriction;
 
 defined('TYPO3') or die();
 
-if (!isset($GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][CustomRestriction::class])) {
-    $GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][CustomRestriction::class] = [];
-}
+$GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][CustomRestriction::class] ??= [];
