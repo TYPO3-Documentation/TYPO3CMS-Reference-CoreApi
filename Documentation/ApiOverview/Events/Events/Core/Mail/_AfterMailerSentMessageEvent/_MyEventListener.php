221ff6a51210eb7543662a0f544c617269cb4897
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-05-08 10:54:04 +0200

    [TASK] Move code snippets into separate files for mail events
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Events/Events/Core/Mail/_AfterMailerSentMessageEvent/_MyEventListener.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Mail\EventListener;
+
+use Psr\Log\LoggerInterface;
+use TYPO3\CMS\Core\Mail\Event\AfterMailerSentMessageEvent;
+use TYPO3\CMS\Core\Mail\Mailer;
+
+final class MyEventListener
+{
+    public function __construct(
+        private readonly LoggerInterface $logger
+    ) {
+    }
+
+    public function __invoke(AfterMailerSentMessageEvent $event): void
+    {
+        $mailer = $event->getMailer();
+        if (!$mailer instanceof Mailer) {
+            return;
+        }
+
+        $sentMessage = $mailer->getSentMessage();
+        if ($sentMessage !== null) {
+            $this->logger->debug($sentMessage->getDebug());
+        }
+    }
+}
