43179f5695cc92a64648ba9f62dfbc883e050ab8
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-05-08 10:54:04 +0200

    [TASK] Move code snippets into separate files for mail events
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Events/Events/Core/Mail/_BeforeMailerSentMessageEvent/_MyEventListener.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Mail\EventListener;
+
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+use TYPO3\CMS\Core\Mail\Event\BeforeMailerSentMessageEvent;
+
+final class MyEventListener
+{
+    public function __invoke(BeforeMailerSentMessageEvent $event): void
+    {
+        $message = $event->getMessage();
+
+        // If $message is an Email implementation, add an additional recipient
+        if ($message instanceof Email) {
+            $message->addCc(new Address('cc_recipient@example.org'));
+        }
+    }
+}
