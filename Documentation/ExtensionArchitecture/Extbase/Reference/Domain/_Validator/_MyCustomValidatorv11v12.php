7dbf0ff016707a58108b281f229facb160853bd8
lina.wolf <lwolf@w-commerce.de>
2023-05-13 20:16:06 +0000

    Important: #96332 - Extbase Validators can use dependency injection
    resolves https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/94
    releases: main, 12.4, 11.5

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/_Validator/_MyCustomValidatorv11v12.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MyVendor\MyExtenion\Validators;
+
+use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
+
+final class MyCustomValidator extends AbstractValidator {
+    public function injectSomething(Something $something)
+    {
+        $this->something = $something;
+    }
+
+    public function setOptions(array $options): void
+    {
+        // This method is upwards compatible with TYPO3 v12, it will be implemented
+        // by AbstractValidator in v12 directly and is part of v12 ValidatorInterface.
+        // @todo: Remove this method when v11 compatibility is dropped.
+        $this->initializeDefaultOptions($options);
+    }
+
+    protected function isValid(mixed $value): void
+    {
+        // TODO: Implement isValid() method.
+    }
+}
