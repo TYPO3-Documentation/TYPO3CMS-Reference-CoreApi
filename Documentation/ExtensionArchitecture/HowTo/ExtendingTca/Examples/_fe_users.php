0ff06c498b69c06123749d41fd2aeaa93a1015d1
lina.wolf <lwolf@w-commerce.de>
2023-05-14 07:58:54 +0000

    [FEATURE] Allow Palettes in 5th parameter of addToAllTCAtypes
    references https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/242

---
+++ Documentation/ExtensionArchitecture/HowTo/ExtendingTca/Examples/_fe_users.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+call_user_func(static function () {
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'fe_users',
+        'tx_myextension_options, tx_myextension_special',
+        '',
+        'after:password'
+    );
+});
