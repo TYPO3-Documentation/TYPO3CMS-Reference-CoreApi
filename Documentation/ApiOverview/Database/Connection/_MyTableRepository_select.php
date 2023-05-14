33871ff14257775877d2ffc6d97d53cbf6c423fb
lina.wolf <lwolf@w-commerce.de>
2023-05-14 09:37:30 +0000

    [FEATURE] Introduce native JSON database field type support
    while at it move the examples into lintable files
    
    references https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/270

---
+++ Documentation/ApiOverview/Database/Connection/_MyTableRepository_select.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Domain\Repository;
+
+use Doctrine\DBAL\Result;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
+final class MyTableRepository
+{
+    private const TABLE_NAME = 'tx_myextension_mytable';
+
+    public function __construct(
+        private readonly ConnectionPool $connectionPool
+    ) {
+    }
+
+    public function countSomething(
+        int $something,
+    ): Result {
+        return $this->connectionPool
+            ->getConnectionForTable(self::TABLE_NAME)
+            ->select(
+                ['*'],
+                self::TABLE_NAME,
+                ['some_value' => $something]
+            );
+    }
+}
