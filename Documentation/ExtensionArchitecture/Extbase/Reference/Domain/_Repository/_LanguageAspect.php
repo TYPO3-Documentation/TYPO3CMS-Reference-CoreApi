5e1bf06a1bf52fd182697ed38e616efffef2580b
lina.wolf <lwolf@w-commerce.de>
2023-05-14 06:35:07 +0000

    [!!!][FEATURE] Use LanguageAspect in QuerySettings for Extbase
    resolves https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/132

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/_Repository/_LanguageAspect.php
@@ -0,0 +1,20 @@
+<?php
+namespace MyVendor\MyExtension\Domain\Repository;
+
+use TYPO3\CMS\Core\Context\LanguageAspect;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+class MyRepository extends Repository
+{
+    public function findSomethingByLanguage(int $languageId, int $contentId) {
+        $query = $this->createQuery();
+        $query->getQuerySettings()->setLanguageAspect(
+            new LanguageAspect(
+                $languageId,
+                $contentId,
+                LanguageAspect::OVERLAYS_MIXED
+            )
+        );
+        // query something
+    }
+}
