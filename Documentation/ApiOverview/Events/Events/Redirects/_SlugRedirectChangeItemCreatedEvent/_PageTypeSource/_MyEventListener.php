b69d4e471877f2436044f9d85aa1927d9be505ad
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-05-15 19:06:20 +0200

    [TASK] Update typo3/coding-standards to 0.7.1
    This avoids deprecations when running the checker, such as:
    
    - Rule "braces" is deprecated. Use "single_space_around_construct", "control_structure_braces", "curly_braces_position", "control_structure_continuation_position", "declare_parentheses", "statement_indentation", "no_multiple_statements_per_line" and "no_extra_blank_lines" instead.
    - Rule "no_trailing_comma_in_singleline_array" is deprecated. Use "no_trailing_comma_in_singleline" instead.
    
    Releases: main, 12.4

--- Documentation/ApiOverview/Events/Events/Redirects/_SlugRedirectChangeItemCreatedEvent/_PageTypeSource/_MyEventListener.php
+++ Documentation/ApiOverview/Events/Events/Redirects/_SlugRedirectChangeItemCreatedEvent/_PageTypeSource/_MyEventListener.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace MyVendor\MyExtension\Backend;
 
 use TYPO3\CMS\Core\Context\Context;
--- Documentation/ExtensionArchitecture/Extbase/Reference/_Annotations/_IgnoreValidation.php
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_Annotations/_IgnoreValidation.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 namespace T3docs\BlogExample\Controller;
 
 use Psr\Http\Message\ResponseInterface;
