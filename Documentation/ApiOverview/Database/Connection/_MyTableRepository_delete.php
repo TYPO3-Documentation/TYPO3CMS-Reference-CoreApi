fc7912d6946ad6a19b9fdb3adaa7a58b16b911aa
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2023-05-16 17:24:42 +0200

    [FEATURE] Introduce native JSON database field type support (#2990)
    * [FEATURE] Introduce native JSON database field type support
    
    while at it move the examples into lintable files
    
    references https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/270
    
    * Update Documentation/ApiOverview/Database/Connection/Index.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/Database/Connection/Index.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/Database/Connection/Index.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Apply changes suggested in Code Review
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Database/Connection/_MyTableRepository_delete.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Domain\Repository;
+
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
+final class MyTableRepository
+{
+    private const TABLE_NAME = 'tx_myextension_mytable';
+
+    public function __construct(
+        private readonly ConnectionPool $connectionPool
+    ) {
+    }
+
+    public function deleteSomething(
+        int $uid
+    ): void {
+        $this->connectionPool
+            ->getConnectionForTable(self::TABLE_NAME)
+            ->delete(
+                self::TABLE_NAME,
+                ['uid' => $uid],
+                [Connection::PARAM_INT]
+            );
+    }
+}
