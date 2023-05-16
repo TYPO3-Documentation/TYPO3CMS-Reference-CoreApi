aab45ab1d593a6ab364c7879d275b0956bdaad51
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-05-16 06:40:47 +0200

    [TASK] Streamline Events/Install chapter (#2997)
    Releases: main, 12.4

--- Documentation/ApiOverview/Events/Events/Install/Index.rst
+++ Documentation/ApiOverview/Events/Events/Install/Index.rst
@@ -1,19 +1,19 @@
-.. include:: /Includes.rst.txt
-.. index:: pair: Events; install
-.. _eventlist-install:
+..  include:: /Includes.rst.txt
+..  index:: pair: Events; install
+..  _eventlist-install:
 
 
-==========================
+=======
 Install
-==========================
+=======
 
 The following list contains :ref:`PSR-14 events <EventDispatcher>`
 in the TYPO3 Core .
 
 **Contents:**
 
-.. toctree::
-   :titlesonly:
-   :glob:
+..  toctree::
+    :titlesonly:
+    :glob:
 
-   *
+    *
---
+++ Documentation/ApiOverview/Events/Events/Install/_ModifyLanguagePacksEvent/_MyEventListener.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Install\EventListener;
+
+use TYPO3\CMS\Install\Service\Event\ModifyLanguagePacksEvent;
+
+final class MyEventListener
+{
+    public function __invoke(ModifyLanguagePacksEvent $event): void
+    {
+        $extensions = $event->getExtensions();
+        foreach ($extensions as $key => $extension) {
+            // Do not download language packs from Core extensions
+            if ($extension['type'] === 'typo3-cms-framework') {
+                $event->removeExtension($key);
+            }
+        }
+
+        // Remove German language pack from EXT:styleguide
+        $event->removeIsoFromExtension('de', 'styleguide');
+    }
+}
