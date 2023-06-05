05ab8faf8d9ab3434997da3348c93e3013d4b729
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-06-05 19:51:48 +0200

    [TASK] Move code snippets from DI chapter into separate files
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/DependencyInjection/_MethodInjectionWithAbstract.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller;
+
+use MyVendor\MyExtension\Logger\Logger;
+use MyVendor\MyExtension\Repository\UserRepository;
+
+abstract class AbstractController
+{
+    protected ?Logger $logger = null;
+
+    public function injectLogger(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+}
+
+final class UserController extends AbstractController
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {
+    }
+}
---
+++ Documentation/ApiOverview/DependencyInjection/_MyClass.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller;
+
+use Psr\Clock\ClockInterface;
+
+final class MyClass
+{
+    public function __construct(
+        private readonly ClockInterface $clock
+    ) {
+    }
+
+    // ...
+}
