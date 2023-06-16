ba285b01084caaa605913b6eeae2e156e52c4d72
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-06-16 12:53:55 +0200

    [TASK] #101074 - Remove deprecated binding of this for selected context menu items (#3126)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/551
    Releases: main

--- Documentation/ApiOverview/Backend/_ContextMenu/_dataset.js
+++
@@ -1,8 +0,0 @@
-ContextMenuActions.renameFile(table, uid, dataset)
-{
-  const actionUrl = dataset.actionUrl;
-  top.TYPO3.Backend.ContentContainer.setUrl(
-    actionUrl + '&target=' + encodeURIComponent(uid) + '&returnUrl='
-    + ContextMenuActions.getReturnUrl()
-  );
-}
