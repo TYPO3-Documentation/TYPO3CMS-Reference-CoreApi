c99b41d10df7a454ed8978a24523cc76a39d9803
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-08-01 19:56:04 +0200

    [TASK] Move snippets into separate files in event dispatcher chapter
    Releases: main, 12.4, 11.5

---
+++ Documentation/ApiOverview/Events/EventDispatcher/_DoingThisAndThatEvent.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Event;
+
+final class DoingThisAndThatEvent
+{
+    public function __construct(
+        private string $mutableProperty,
+        private readonly int $immutableProperty,
+    ) {
+    }
+
+    public function getMutableProperty(): string
+    {
+        return $this->mutableProperty;
+    }
+
+    public function setMutableProperty(string $mutableProperty): void
+    {
+        $this->mutableProperty = $mutableProperty;
+    }
+
+    public function getImmutableProperty(): int
+    {
+        return $this->immutableProperty;
+    }
+}
