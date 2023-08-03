9fec54b3535788141ffd746608ed4f25faa18725
lina.wolf <lwolf@w-commerce.de>
2023-08-03 07:47:06 +0000

    [TASK] Move ExtensionManagementUtility::addUserTSConfig

--- Documentation/ApiOverview/PageTypes/_pages.php
+++ Documentation/ApiOverview/PageTypes/_pages.php
@@ -2,15 +2,18 @@
 
 defined('TYPO3') or die();
 
-// SAME as registered in ext_tables.php
-$customPageDoktype = 116;
-$customIconClass = 'tx_examples-archive-page';
+// encapsulate all locally defined variables
+(function () {
+    // SAME as registered in ext_tables.php
+    $customPageDoktype = 116;
+    $customIconClass = 'tx_examples-archive-page';
 
-// Add the new doktype to the page type selector
-$GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
-    'LLL:EXT:examples/Resources/Private/Language/locallang.xlf:archive_page_type',
-    $customPageDoktype,
-    $customIconClass,
-];
-// Add the icon to the icon class configuration
-$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][$customPageDoktype] = $customIconClass;
+    // Add the new doktype to the page type selector
+    $GLOBALS['TCA']['pages']['columns']['doktype']['config']['items'][] = [
+        'LLL:EXT:examples/Resources/Private/Language/locallang.xlf:archive_page_type',
+        $customPageDoktype,
+        $customIconClass,
+    ];
+    // Add the icon to the icon class configuration
+    $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes'][$customPageDoktype] = 'tx_examples-archive-page';
+})();
