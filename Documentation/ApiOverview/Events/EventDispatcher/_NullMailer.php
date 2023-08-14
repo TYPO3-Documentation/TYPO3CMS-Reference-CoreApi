01c9880cb93702660f1eef5bd1ee3437b8278ea4
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-08-14 13:23:48 +0200

    [FEATURE] #101544 - Introduce PHP attribute to autoconfigure event listeners (#3361)
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/624
    Releases: main

--- Documentation/ApiOverview/Events/EventDispatcher/_NullMailer.php
+++ Documentation/ApiOverview/Events/EventDispatcher/_NullMailer.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace MyVendor\MyExtension\EventListener;
 
+use TYPO3\CMS\Core\Attribute\AsEventListener;
 use TYPO3\CMS\Core\Mail\Event\AfterMailerInitializationEvent;
 
 final class NullMailer
 {
+    #[AsEventListener(
+        identifier: 'my-extension/null-mailer',
+        before: 'someIdentifier, anotherIdentifier',
+    )]
     public function __invoke(AfterMailerInitializationEvent $event): void
     {
         $event->getMailer()->injectMailSettings(['transport' => 'null']);
