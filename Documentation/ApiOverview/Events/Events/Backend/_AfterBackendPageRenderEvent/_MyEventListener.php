3166afa366a80dc33966725ed96d79e2fbbaa9c8
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-08-14 18:45:44 +0200

    [TASK] #101544 - Use PHP attribute to tag event listeners for event examples
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/624
    Releases: main

--- Documentation/ApiOverview/Events/Events/Backend/_AfterBackendPageRenderEvent/_MyEventListener.php
+++ Documentation/ApiOverview/Events/Events/Backend/_AfterBackendPageRenderEvent/_MyEventListener.php
@@ -5,9 +5,13 @@ declare(strict_types=1);
 namespace MyVendor\MyExtension\Backend\EventListener;
 
 use TYPO3\CMS\Backend\Controller\Event\AfterBackendPageRenderEvent;
+use TYPO3\CMS\Core\Attribute\AsEventListener;
 
 final class MyEventListener
 {
+    #[AsEventListener(
+        identifier: 'my-extension/backend/after-backend-page-render',
+    )]
     public function __invoke(AfterBackendPageRenderEvent $event): void
     {
         $content = $event->getContent() . ' I was here';
