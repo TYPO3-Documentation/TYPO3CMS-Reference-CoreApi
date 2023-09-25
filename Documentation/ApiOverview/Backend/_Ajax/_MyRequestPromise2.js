f36ca146be6fb8aeda4735214ed8a66bbeff4557
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-09-25 20:47:29 +0200

    [TASK] Extract JavaScript snippets from "Ajax, client-side" chapter
    Additionally, switch to ES6 modules. Also the ResponseError is gone with v12.
    
    This is a preparation for merging the "Ajax" chapter with the "Ajax, client-side" chapter.
    
    Related: https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-CoreApi/issues/2331
    Related: https://review.typo3.org/c/Packages/TYPO3.CMS/+/65625/4/typo3/sysext/core/Documentation/Changelog/10.3/Feature-89738-ApiForAjaxRequests.rst#4
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Backend/_Ajax/_MyRequestPromise2.js
@@ -0,0 +1,15 @@
+import AjaxRequest from "@typo3/core/ajax/ajax-request.js";
+
+let request = new AjaxRequest('https://example.org/my-endpoint');
+
+const json = {foo: 'bar'};
+let promise = request.post(json, {
+  headers: {
+    'Content-Type': 'application/json; charset=utf-8'
+  }
+});
+
+promise.then(async function (response) {
+  const responseText = await response.resolve();
+  console.log(responseText);
+});
