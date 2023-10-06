2162b6fb26ca97848272568f34425ef2cc0f42e2
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-10-06 08:28:06 +0200

    [FEATURE] #101970 - Ajax API accepts native URL and URLSearchParams objects as arguments (#3519)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/665
    Releases: main

--- Documentation/ApiOverview/Backend/JavaScript/AjaxRequest/_MyRequest2.js
+++ Documentation/ApiOverview/Backend/JavaScript/AjaxRequest/_MyRequest2.js
@@ -2,12 +2,20 @@ import AjaxRequest from "@typo3/core/ajax/ajax-request.js";
 
 let request = new AjaxRequest('https://example.org/my-endpoint');
 
-const qs = {
+let queryArguments = {
   foo: 'bar',
   bar: {
     baz: ['foo', 'bencer']
   }
 };
-request = request.withQueryArguments(qs);
+// or:
+let queryArguments = new URLSearchParams({
+  foo: 'bar',
+  baz: {
+    baz: ['foo', 'bencer']
+  }
+});
+
+request = request.withQueryArguments(queryArguments);
 
 // The query string compiles to ?foo=bar&bar[baz][0]=foo&bar[baz][1]=bencer
