91117799ea23005435e9d1b36f980507a1d2f816
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-11-07 18:48:52 +0100

    [TASK] Revise FAL / Using Fal / The StorageRepository class chapter (#3682)
    - Add references where appropriate
    - Move code snippets into dedicated folder
    - Add captions to code snippets
    - Move information of note into the class for more resiliency.
    
    Releases: main, 12.4, 11.5

--- Documentation/ApiOverview/Fal/UsingFal/_ExamplesStorageRepository/_GetStorageObjectExample.php
+++ Documentation/ApiOverview/Fal/UsingFal/_ExamplesStorageRepository/_GetStorageObjectExample.php
@@ -2,18 +2,20 @@
 
 declare(strict_types=1);
 
+namespace MyVendor\MyExtension\Classes\Resource;
+
 use TYPO3\CMS\Core\Resource\StorageRepository;
 
-class GetStorageObjectExample
+final class GetStorageObjectExample
 {
     public function __construct(
-        private readonly StorageRepository $storageRepository
+        private readonly StorageRepository $storageRepository,
     ) {}
 
     public function doSomething(): void
     {
         $storage = $this->storageRepository->getStorageObject(3);
 
-        // .. more logic
+        // ... more logic
     }
 }
