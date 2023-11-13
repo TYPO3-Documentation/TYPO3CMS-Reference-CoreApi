1ab5bc030f59e87796c18f342dbabdc57e5f4b8d
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-11-13 14:27:39 +0100

    [TASK] Overhaul deprecation chapter
    This change describes the current state of enabling/disabling deprecations as introduced with TYPO3 v9.5!
    
    Related: https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/9.5.x/Important-84105-StreamlineDeprecationLogHandling.html
    Releases: main, 12.4, 11.5

---
+++ Documentation/ApiOverview/Deprecation/_settings.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // ... some configuration
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    // ... more configuration
+];
