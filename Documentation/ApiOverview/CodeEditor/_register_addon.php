757023439d53412d87f2a0d06fa47875f9a84a3f
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-12-08 16:32:21 +0000

    [TASK] #102440 - Merge EXT:t3editor into backend (#3752)
    This is mainly a copy of the documentation of the t3editor system extension. Further
    adjustments can be made in subsequent changes.
    
    Note: The TCA render type changed from "t3editor" to "codeEditor". However,
    the configuration of custom addons and modes is still done in the
    Configuration/Backend/T3editor/ folder.
    
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/734
    Releases: main

---
+++ Documentation/ApiOverview/CodeEditor/_register_addon.php
@@ -0,0 +1,17 @@
+<?php
+
+use TYPO3\CMS\Core\Page\JavaScriptModuleInstruction;
+
+return [
+    'my/addon' => [
+        'module' => JavaScriptModuleInstruction::create(
+            '@codemirror/addon',
+            'addon'
+        )->invoke(),
+        'cssFiles' => [
+            'EXT:my_extension/Resources/Public/Css/MyAddon.css',
+        ],
+        'options' => ['foobar' => 'baz'],
+    ],
+    'modes' => ['htmlmixed', 'xml'],
+];
