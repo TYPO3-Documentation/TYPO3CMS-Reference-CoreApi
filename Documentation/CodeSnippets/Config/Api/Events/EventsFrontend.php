e44a91aeb5664707febd04fcd0d1d888e195f97d
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-12-15 08:00:15 +0000

    [FEATURE] #102581 - Add PSR-14 event for manipulating ContentObjectRenderer (#3780)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/744
    Releases: main

--- Documentation/CodeSnippets/Config/Api/Events/EventsFrontend.php
+++ Documentation/CodeSnippets/Config/Api/Events/EventsFrontend.php
@@ -75,4 +75,10 @@ return [
         'targetFileName' => 'CodeSnippets/Events/Frontend/AfterCachedPageIsPersistedEvent.rst.txt',
         'withCode' => false,
     ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Frontend\ContentObject\Event\AfterContentObjectRendererInitializedEvent::class,
+        'targetFileName' => 'CodeSnippets/Events/Frontend/AfterContentObjectRendererInitializedEvent.rst.txt',
+        'withCode' => false,
+    ],
 ];
