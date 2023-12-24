e73ee7bea53ab035acb2b652d077b99e8390fc2e
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2023-12-24 11:26:02 +0100

    [TASK] Streamline "Backend module configuration" chapter
    Mainly:
    - Move snippets into separate files
    - Remove "Changed in version 12.2" as this is superfluous now
    - Add named anchors and use them to leek to other confvals
    - Strip header with version number from screenshot to be "timeless"
    
    Releases: main, 12.4

---
+++ Documentation/ExtensionArchitecture/HowTo/BackendModule/_ModuleConfiguration/_AliasIdentifier.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'file_editcontent' => [
+        // ...
+        'path' => '/file/editcontent',
+        'aliases' => ['file_edit'],
+    ],
+];
---
+++ Documentation/ExtensionArchitecture/HowTo/BackendModule/_ModuleConfiguration/_AliasModule.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'workspaces_admin' => [
+        'parent' => 'web',
+        // ...
+        // choose the previous name or an alternative name
+        'aliases' => ['web_WorkspacesWorkspaces'],
+    ],
+];
