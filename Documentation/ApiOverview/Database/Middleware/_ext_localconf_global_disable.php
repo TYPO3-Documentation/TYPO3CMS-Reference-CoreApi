b50c662c6095b5bc3dbc04d7e475830d847579b1
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-05 19:58:43 +0100

    [FEATURE] #102586 - Introduce sortable Doctrine DBAL middleware registration
    Additionally, the global middleware section is moved before the connection middleware section.
    
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/755
    Releases: main

--- Documentation/ApiOverview/Database/Middleware/_ext_localconf_global_disable.php
+++ Documentation/ApiOverview/Database/Middleware/_ext_localconf_global_disable.php
@@ -7,9 +7,12 @@ use MyVendor\MyExtension\Doctrine\Driver\CustomGlobalDriverMiddleware;
 defined('TYPO3') or die();
 
 // Register a global middleware
-$GLOBALS['TYPO3_CONF_VARS']['DB']['globalDriverMiddlewares']['my-ext/custom-global-driver-middleware']
-    = CustomGlobalDriverMiddleware::class;
+$GLOBALS['TYPO3_CONF_VARS']['DB']['globalDriverMiddlewares']['my-ext/custom-global-driver-middleware'] = [
+    'target' => CustomGlobalDriverMiddleware::class,
+    'after' => [
+        'typo3/core/custom-platform-driver-middleware',
+    ],
+];
 
 // Disable a global driver middleware for a specific connection
-$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['SecondDatabase']['driverMiddlewares']['my-ext/custom-global-driver-middleware']
-    = '';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['SecondDatabase']['driverMiddlewares']['my-ext/custom-global-driver-middleware']['disabled'] = true;
