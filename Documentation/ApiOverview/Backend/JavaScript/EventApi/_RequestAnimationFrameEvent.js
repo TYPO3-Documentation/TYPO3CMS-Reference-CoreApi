48522d8d6733c48fa5a00600a74789e9d8ea9970
ServerKnights <61573991+serverKnights@users.noreply.github.com>
2024-01-18 09:43:44 +0100

    Adjustment in the ES6 JavaScript EventApi (#3895)

--- Documentation/ApiOverview/Backend/JavaScript/EventApi/_RequestAnimationFrameEvent.js
+++ Documentation/ApiOverview/Backend/JavaScript/EventApi/_RequestAnimationFrameEvent.js
@@ -1,4 +1,4 @@
-import RequestAnimationFrameEvent from '@typo3/core/event/request-animation-frame-event';
+import RequestAnimationFrameEvent from '@typo3/core/event/request-animation-frame-event.js';
 
 new RequestAnimationFrameEvent('mousewheel', function (e) {
   console.log('Triggered every 16ms (= 60 FPS)!');
