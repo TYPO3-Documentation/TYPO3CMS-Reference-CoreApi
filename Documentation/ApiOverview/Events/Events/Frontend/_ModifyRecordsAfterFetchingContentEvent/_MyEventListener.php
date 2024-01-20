32aa120ded2e4f7758d2aa4f0fd8a80468962d72
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-20 06:24:29 +0100

    [FEATURE] #99323 - PSR-14 Event for modifying records after fetching content (#3893)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/789
    Releases: main

---
+++ Documentation/ApiOverview/Events/Events/Frontend/_ModifyRecordsAfterFetchingContentEvent/_MyEventListener.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Frontend\EventListener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Frontend\ContentObject\Event\ModifyRecordsAfterFetchingContentEvent;
+
+#[AsEventListener(
+    identifier: 'my-extension/my-event-listener'
+)]
+final class MyEventListener
+{
+    public function __invoke(ModifyRecordsAfterFetchingContentEvent $event): void
+    {
+        if ($event->getConfiguration()['table'] !== 'tt_content') {
+            return;
+        }
+
+        $records = array_reverse($event->getRecords());
+        $event->setRecords($records);
+    }
+}
