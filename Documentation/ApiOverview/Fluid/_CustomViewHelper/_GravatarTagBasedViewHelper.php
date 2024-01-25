2ecde410d3ba85fc1015474d0cc925d30307fc8b
lina.wolf <lwolf@w-commerce.de>
2024-01-25 14:41:01 +0100

    [TASK] Overhaul Developing a custom ViewHelper
    - Use "my_extension" instead of "blog_example"
    - Normalize indentation
    - move code examples to includes
    - use typed returns, final and strict types
    - Update line numbers where they are refered
    - AbstractTagBasedViewHelper::handleAdditionalArguments changed in the Core, however I am not sure what this section wants to say. Lets deal with it in a follow up?

---
+++ Documentation/ApiOverview/Fluid/_CustomViewHelper/_GravatarTagBasedViewHelper.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper;
+
+final class GravatarViewHelper extends AbstractTagBasedViewHelper
+{
+    protected $tagName = 'img';
+
+    public function initializeArguments(): void
+    {
+        parent::initializeArguments();
+        $this->registerUniversalTagAttributes();
+        $this->registerTagAttribute('alt', 'string', 'Alternative Text for the image');
+        $this->registerArgument('emailAddress', 'string', 'The email address to resolve the gravatar for', true);
+    }
+
+    public function render(): string
+    {
+        $this->tag->addAttribute(
+            'src',
+            'http://www.gravatar.com/avatar/' . md5($this->arguments['emailAddress'])
+        );
+        return $this->tag->render();
+    }
+}
