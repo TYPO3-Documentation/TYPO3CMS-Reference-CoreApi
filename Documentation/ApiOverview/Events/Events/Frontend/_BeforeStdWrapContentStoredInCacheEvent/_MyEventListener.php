7bf1a30fe64b8eee60f9e360f35d2c73df505580
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-26 13:06:47 +0000

    [FEATURE] #102849 - PSR-14 event for manipulating store cache functionality of stdWrap (#3939)
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/798
    Releases: main

---
+++ Documentation/ApiOverview/Events/Events/Frontend/_BeforeStdWrapContentStoredInCacheEvent/_MyEventListener.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Frontend\EventListener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Frontend\ContentObject\Event\BeforeStdWrapContentStoredInCacheEvent;
+
+#[AsEventListener(
+    identifier: 'my-extension/before-stdwrap-content-stored-in-cache'
+)]
+final class MyEventListener
+{
+    public function __invoke(BeforeStdWrapContentStoredInCacheEvent $event): void
+    {
+        if (in_array('foo', $event->getTags(), true)) {
+            $event->setContent('modified-content');
+        }
+    }
+}
