d60d634874dd2444e110b2ad209a7771bc130556
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-01-26 10:46:04 +0100

    [TASK] Overhaul Developing a custom ViewHelper (#3931)
    * [TASK] Overhaul Developing a custom ViewHelper
    
    - Use "my_extension" instead of "blog_example"
    - Normalize indentation
    - move code examples to includes
    - use typed returns, final and strict types
    - Update line numbers where they are refered
    - AbstractTagBasedViewHelper::handleAdditionalArguments changed in the Core, however I am not sure what this section wants to say. Lets deal with it in a follow up?
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Fluid/_CustomViewHelper/_StrtolowerViewHelper.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\BlogExample\ViewHelpers;
+
+use TYPO3Fluid\Fluid\Core\Compiler\TemplateCompiler;
+use TYPO3Fluid\Fluid\Core\Parser\SyntaxTree\ViewHelperNode;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
+final class StrtolowerViewHelper extends AbstractViewHelper
+{
+    use CompileWithRenderStatic;
+
+    public function initializeArguments(): void
+    {
+        $this->registerArgument('string', 'string', 'The string to lowercase.', true);
+    }
+
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ): string {
+        return strtolower($arguments['string']);
+    }
+
+    public function compile(
+        $argumentsName,
+        $closureName,
+        &$initializationPhpCode,
+        ViewHelperNode $node,
+        TemplateCompiler $compiler
+    ): string {
+        return 'strtolower(' . $argumentsName . '[\'string\'])';
+    }
+}
