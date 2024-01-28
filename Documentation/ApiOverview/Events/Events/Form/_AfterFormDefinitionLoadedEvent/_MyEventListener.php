607b2421529627c004e211288c07e7d7d6cab7c1
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-01-28 08:35:26 +0000

    [FEATURE] #102865 - PSR-14 event for modifying loaded form definition (#3951)
    Related: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/812
    Releases: main

---
+++ Documentation/ApiOverview/Events/Events/Form/_AfterFormDefinitionLoadedEvent/_MyEventListener.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\LinkHandling\EventListener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Form\Mvc\Persistence\Event\AfterFormDefinitionLoadedEvent;
+
+#[AsEventListener(
+    identifier: 'my-extension/after-form-definition-loaded'
+)]
+final class MyEventListener
+{
+    public function __invoke(AfterFormDefinitionLoadedEvent $event): void
+    {
+        if ($event->getPersistenceIdentifier() === '1:/form_definitions/contact.form.yaml') {
+            $formDefinition = $event->getFormDefinition();
+            $formDefinition['label'] = 'Some new label';
+            $event->setFormDefinition($formDefinition);
+        }
+    }
+}
---
+++ Documentation/CodeSnippets/Config/Api/Events/EventsForm.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Form\Mvc\Persistence\Event\AfterFormDefinitionLoadedEvent::class,
+        'targetFileName' => 'CodeSnippets/Events/Form/AfterFormDefinitionLoadedEvent.rst.txt',
+        'withCode' => false,
+    ],
+];
