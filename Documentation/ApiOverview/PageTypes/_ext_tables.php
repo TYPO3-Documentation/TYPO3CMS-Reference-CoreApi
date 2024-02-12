41abf4840170dedf3350866ce5ad0d9e82fe5d82
ghermens <47635467+ghermens@users.noreply.github.com>
2024-02-12 07:35:33 +0100

    [BUGFIX] Remove unused use statement

--- Documentation/ApiOverview/PageTypes/_ext_tables.php
+++ Documentation/ApiOverview/PageTypes/_ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 
 use TYPO3\CMS\Core\DataHandling\PageDoktypeRegistry;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') or die();
