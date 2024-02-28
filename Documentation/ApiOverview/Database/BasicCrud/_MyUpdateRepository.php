3f29ed549488480d1ef4ceef3db8ebac64860e86
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-02-28 21:06:59 +0100

    Adjust indentation

--- Documentation/ApiOverview/Database/BasicCrud/_MyUpdateRepository.php
+++ Documentation/ApiOverview/Database/BasicCrud/_MyUpdateRepository.php
@@ -18,7 +18,7 @@ final class MyUpdateRepository
             ->update(
                 'tt_content',
                 [ 'bodytext' => 'ipsum' ], // set
-                [ 'bodytext' => 'lorem' ],  // where
+                [ 'bodytext' => 'lorem' ], // where
             );
     }
 }
