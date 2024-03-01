8ac29e8d41b6a67cb17b201ec4aaccf327412fd3
Garvin Hicking <blog@garv.in>
2024-03-01 13:46:14 +0100

    Apply suggestions from code review
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

--- Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_ExternalRedirectController.php
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_FrontendPlugin/_ExternalRedirectController.php
@@ -3,7 +3,6 @@
 use Psr\Http\Message\ResponseInterface;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
-// Example by: https://brot.krue.ml/extbase-controller-action-responses-in-typo3/
 final class MyController extends ActionController
 {
     public function downloadAction(): ResponseInterface
