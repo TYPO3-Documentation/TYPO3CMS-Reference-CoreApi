cabe8b17da14812428a2b698f4afe6cf2940d5b6
Christian Weiske <weiske@mogic.com>
2024-03-07 12:07:54 +0100

    [TASK] Use "-" instead of "_" in page type icon name (#4126)
    So that the naming is consistent the other examples:
    - Documentation/ApiOverview/Icon/Index.rst
    - Documentation/ApiOverview/Icon/_Icons.php
    
    Those use "tx-myext-...icon" and not "tx_myext-...icon".

--- Documentation/ApiOverview/PageTypes/_Icons.php
+++ Documentation/ApiOverview/PageTypes/_Icons.php
@@ -3,7 +3,7 @@
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
 
 return [
-    'tx_examples-archive-page' => [
+    'tx-examples-archive-page' => [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:examples/Resources/Public/Images/ArchivePage.svg',
     ],
