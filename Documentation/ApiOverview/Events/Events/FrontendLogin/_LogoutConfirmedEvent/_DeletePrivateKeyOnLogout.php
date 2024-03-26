37473369d6cc8bcc5fe8693a9af375269a7fe549
lina.wolf <lwolf@w-commerce.de>
2024-03-26 07:17:30 +0100

    [TASK] Add example for LogoutConfirmedEvent listener
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Events/Events/FrontendLogin/_LogoutConfirmedEvent/_DeletePrivateKeyOnLogout.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MyVendor\MyExtension\EventListener;
+
+use MyVendor\MyExtension\KeyPairHandling\KeyFileService;
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Core\Context\Context;
+use TYPO3\CMS\Core\Context\UserAspect;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
+use TYPO3\CMS\FrontendLogin\Event\LogoutConfirmedEvent;
+
+#[AsEventListener(
+    identifier: 'my-extension/delete-private-key-on-logout',
+    before: 'someIdentifier, anotherIdentifier',
+)]
+final class LogoutEventListener
+{
+    public function __construct(
+        private readonly KeyFileService $keyFileService,
+        protected readonly Context $context,
+    ) {}
+
+    public function __invoke(LogoutConfirmedEvent $event): void
+    {
+        $userAspect = $this->context->getAspect('frontend.user');
+        assert($userAspect instanceof UserAspect);
+        if ($this->keyFileService->deletePrivateKey($userAspect)) {
+            $event->getController()->addFlashMessage('Your private key has been deleted. ', '', ContextualFeedbackSeverity::NOTICE);
+        } else {
+            $event->getController()->addFlashMessage('Deletion of your private key failed. It will be deleted automatically withing 15 minutes by a scheduler task. ', '', ContextualFeedbackSeverity::WARNING);
+        }
+    }
+}
