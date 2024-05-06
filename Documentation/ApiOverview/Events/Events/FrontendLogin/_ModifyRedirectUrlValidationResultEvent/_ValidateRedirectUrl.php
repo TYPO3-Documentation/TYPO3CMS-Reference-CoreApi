11bc26a7ed09e75d82dc0f42b030b9c45ca60864
lina.wolf <lwolf@w-commerce.de>
2024-05-06 10:07:06 +0200

    [FEATURE] ModifyRedirectUrlValidationResultEvent
    Releases: main
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/911

--- Documentation/ApiOverview/Events/Events/FrontendLogin/_ModifyRedirectUrlValidationResultEvent/_ValidateRedirectUrl.php
+++ Documentation/ApiOverview/Events/Events/FrontendLogin/_ModifyRedirectUrlValidationResultEvent/_ValidateRedirectUrl.php
@@ -9,7 +9,7 @@ use TYPO3\CMS\FrontendLogin\Event\ModifyRedirectUrlValidationResultEvent;
 
 final class ValidateRedirectUrl
 {
-    const TRUSTED_HOST_FOR_REDIRECT = 'example.org';
+    public const TRUSTED_HOST_FOR_REDIRECT = 'example.org';
 
     #[AsEventListener('validate-custom-redirect-url')]
     public function __invoke(ModifyRedirectUrlValidationResultEvent $event): void
