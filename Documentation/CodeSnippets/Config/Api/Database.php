2a1b855a76f30471677f227bb06083ce1329421e
lina.wolf <lwolf@w-commerce.de>
2024-05-13 14:17:16 +0200

    [TASK] Embed API for expression builder (#4402)
    * [TASK] Embed API for various expression builder expressions (#4243)
    
    Releases: main, 12.4
    
    * [TASK] Embed API for expression builder expressions (#4243)
    
    Releases: main, 12.4
    
    * [TASK] Embed API for expression builder expressions (#4243)
    
    Releases: main, 12.4
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    (cherry picked from commit d869f6046bad626802cdf6e8a33da61351d7e613)

--- Documentation/CodeSnippets/Config/Api/Database.php
+++ Documentation/CodeSnippets/Config/Api/Database.php
@@ -54,4 +54,57 @@ return [
         ],
         'withCode' => false,
     ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder::class,
+        'targetFileName' => 'ApiOverview/Database/ExpressionBuilder/_ExpressionBuilderLength.rst.txt',
+        'includeClassComment' => false,
+        'members' => [
+            'length',
+        ],
+        'withCode' => false,
+        'noindexInClass' => true,
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder::class,
+        'targetFileName' => 'ApiOverview/Database/ExpressionBuilder/_ExpressionBuilderTrim.rst.txt',
+        'includeClassComment' => false,
+        'members' => [
+            'trim',
+        ],
+        'withCode' => false,
+        'noindexInClass' => true,
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder::class,
+        'targetFileName' => 'ApiOverview/Database/ExpressionBuilder/_ExpressionBuilderAggregate.rst.txt',
+        'includeClassComment' => false,
+        'members' => [
+            'min', 'max', 'avg', 'sum', 'count',
+        ],
+        'withCode' => false,
+        'noindexInClass' => true,
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder::class,
+        'targetFileName' => 'ApiOverview/Database/ExpressionBuilder/_ExpressionBuilderComparisons.rst.txt',
+        'includeClassComment' => false,
+        'members' => [
+            'eq', 'neq', 'lt', 'lte', 'gt', 'gte', 'isNull', 'isNotNull', 'like', 'notLike', 'in', 'notIn', 'inSet', 'notInSet', 'bitAnd',
+        ],
+        'withCode' => false,
+        'noindexInClass' => true,
+    ],
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder::class,
+        'targetFileName' => 'ApiOverview/Database/ExpressionBuilder/_ExpressionBuilder.rst.txt',
+        'members' => [
+            '__construct',
+        ],
+        'withCode' => false,
+    ],
 ];
