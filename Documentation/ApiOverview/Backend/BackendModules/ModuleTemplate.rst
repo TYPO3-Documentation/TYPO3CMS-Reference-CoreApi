7fb4d0934bb74bd3a15c203edf6450d12060d2bf
lina.wolf <lwolf@w-commerce.de>
2024-05-17 12:42:10 +0200

    [TASK] Describe Using ModuleTemplate
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Backend/BackendModules/ModuleTemplate.rst
@@ -0,0 +1,28 @@
+.. include:: /Includes.rst.txt
+.. index:: Backend modules; ModuleTemplateFactory
+.. _ModuleTemplate:
+
+==============
+ModuleTemplate
+==============
+
+Backend controllers should use :ref:`ModuleTemplateFactory:create <ModuleTemplateFactory>`
+to create instances of a :php:`\TYPO3\CMS\Backend\Template\ModuleTemplate.`.
+
+..  include:: _ModuleTemplate.rst.txt
+
+
+.. _ModuleTemplate-examples:
+
+Example: Initialize module template
+===================================
+
+.. seealso::
+    :ref:`Create a backend module with Core functionality <t3coreapi:backend-modules-template-without-extbase>`
+    and :ref:`Create a backend module with Extbase <t3coreapi:backend-modules-template>`.
+
+API functions of the ModuleTemplate can be used to add buttons to the button bar
+
+..  literalinclude:: _BackendModuleController_ModuleTemplate.php
+    :language: php
+    :caption: EXT:my_extension/Classes/Controller/BackendModuleController.php
---
+++ Documentation/ApiOverview/Backend/BackendModules/_BackendModuleController_ModuleTemplate.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Backend\Template\ModuleTemplate;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Localization\LanguageService;
+use TYPO3\CMS\Core\Type\ContextualFeedbackSeverity;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+final class BackendModuleController extends ActionController
+{
+    public function __construct(
+        private readonly ModuleTemplateFactory $moduleTemplateFactory,
+        // ..
+    ) {}
+
+    private function initializeModuleTemplate(ServerRequestInterface $request): ModuleTemplate
+    {
+        $moduleTemplate = $this->moduleTemplateFactory->create($request);
+        // Add common buttons and menues
+
+        $buttonBar = $moduleTemplate->getDocHeaderComponent()->getButtonBar();
+        $this->addButtonsToBar($buttonBar);
+
+        $menu = $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->makeMenu();
+        $menu = $this->addMenuItems($menu);
+        $moduleTemplate->getDocHeaderComponent()->getMenuRegistry()->addMenu($menu);
+
+        // Add menu items
+
+        return $moduleTemplate;
+    }
+    private function getLanguageService(): LanguageService
+    {
+        return $GLOBALS['LANG'];
+    }
+
+    private function addButtonsToBar(\TYPO3\CMS\Backend\Template\Components\ButtonBar $buttonBar): void
+    {
+        // TODO: Add buttons to bar
+    }
+
+    private function addMenuItems(\TYPO3\CMS\Backend\Template\Components\Menu\Menu $menu): \TYPO3\CMS\Backend\Template\Components\Menu\Menu
+    {
+        // TODO: Add menu items
+        return $menu;
+    }
+
+    public function someAction(): ResponseInterface
+    {
+        $moduleTemplate = $this->initializeModuleTemplate($this->request);
+
+        // Add a flash message
+        $moduleTemplate->addFlashMessage('Everything is fine', 'Info', ContextualFeedbackSeverity::INFO);
+
+        // Assign variables to the template
+        $moduleTemplate->assignMultiple([
+            'variable1' => 'value 1',
+            'variable2' => 'value 2',
+        ]);
+        return $moduleTemplate->renderResponse('Backend/Some');
+    }
+
+}
