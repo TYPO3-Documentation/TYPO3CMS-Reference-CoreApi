c697264797852693f02ad5c8b4e7c9d0e8a5a50a
lina.wolf <lwolf@w-commerce.de>
2024-05-23 16:56:00 +0200

    [BUGFIX] Do not reuse DataHandler
    Releases: main, 12.4, 11.5

--- Documentation/ApiOverview/DataHandler/Database/_BasicUsage.php
+++ Documentation/ApiOverview/DataHandler/Database/_BasicUsage.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 namespace MyVendor\MyExtension\DataHandling;
 
 use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class MyClass
 {
-    public function __construct(
-        private readonly DataHandler $dataHandler,
-    ) {}
-
     public function basicUsage(): void
     {
+        /** @var DataHandler $dataHandler */
+        // Do not inject or reuse the DataHander as it holds state!
+        // Do not use `new` as GeneralUtility::makeInstance handles dependencies
+        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+
         $cmd = [];
         $data = [];
-        $this->dataHandler->start($data, $cmd);
+        $dataHandler->start($data, $cmd);
 
         // ... do something more ...
     }
--- Documentation/ApiOverview/DataHandler/UsingDataHandler/_SubmitData.php
+++ Documentation/ApiOverview/DataHandler/UsingDataHandler/_SubmitData.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace MyVendor\MyExtension\Classes\DataHandling;
 
 use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class MyClass
 {
-    public function __construct(
-        private readonly DataHandler $dataHandler,
-    ) {}
-
     public function submitData(): void
     {
         // Prepare the data array
@@ -19,11 +16,16 @@ final class MyClass
             // ... the data ...
         ];
 
+        /** @var DataHandler $dataHandler */
+        // Do not inject or reuse the DataHander as it holds state!
+        // Do not use `new` as GeneralUtility::makeInstance handles dependencies
+        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+
         // Register the $data array inside DataHandler and initialize the
         // class internally.
-        $this->dataHandler->start($data, []);
+        $dataHandler->start($data, []);
 
         // Submit data and have all records created/updated.
-        $this->dataHandler->process_datamap();
+        $dataHandler->process_datamap();
     }
 }
