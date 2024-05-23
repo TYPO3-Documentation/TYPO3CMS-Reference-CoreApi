e26166f70ccaa69c65b11eb64a88c453e1a4f49c
lina.wolf <lwolf@w-commerce.de>
2024-05-23 16:56:00 +0200

    [BUGFIX] Do not reuse DataHandler
    Releases: main, 12.4, 11.5

--- Documentation/ApiOverview/DataHandler/UsingDataHandler/_ErrorHandling.php
+++ Documentation/ApiOverview/DataHandler/UsingDataHandler/_ErrorHandling.php
@@ -6,21 +6,26 @@ namespace MyVendor\MyExtension\Classes\DataHandling;
 
 use Psr\Log\LoggerInterface;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class MyClass
 {
     public function __construct(
-        private readonly DataHandler $dataHandler,
         private readonly LoggerInterface $logger,
     ) {}
 
     public function handleError(): void
     {
+        /** @var DataHandler $dataHandler */
+        // Do not inject or reuse the DataHander as it holds state!
+        // Do not use `new` as GeneralUtility::makeInstance handles dependencies
+        $dataHandler = GeneralUtility::makeInstance(DataHandler::class);
+
         // ... previous call of DataHandler's process_datamap() or process_cmdmap()
 
-        if ($this->dataHandler->errorLog !== []) {
+        if ($dataHandler->errorLog !== []) {
             $this->logger->error('Error(s) while creating content element');
-            foreach ($this->dataHandler->errorLog as $log) {
+            foreach ($dataHandler->errorLog as $log) {
                 // handle error, for example, in a log
                 $this->logger->error($log);
             }
