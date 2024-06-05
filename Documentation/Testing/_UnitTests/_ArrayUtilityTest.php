b9ef1487fefdb7018c1b18520b4da30dc24fe1ed
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-06-05 08:23:06 +0200

    [TASK] Modernize Unit test examples (#4491)
    * [TASK] Modernize Unit test examples
    
    Releases: main, 12.4
    
    * Update Documentation/Testing/WritingUnit.rst
    
    Co-authored-by: Garvin Hicking <blog@garv.in>
    
    ---------
    
    Co-authored-by: Garvin Hicking <blog@garv.in>

--- Documentation/Testing/_UnitTests/_ArrayUtilityTest.php
+++ Documentation/Testing/_UnitTests/_ArrayUtilityTest.php
@@ -2,21 +2,23 @@
 
 namespace TYPO3\CMS\Core\Tests\Unit\Utility;
 
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
-class ArrayUtilityTest extends UnitTestCase
+final class ArrayUtilityTest extends UnitTestCase
 {
-    private ArrayUtility $subject;
-
-    //...
-
-    /**
-     * @test
-     * @dataProvider filterByValueRecursive
-     */
-    public function filterByValueRecursiveCorrectlyFiltersArray($needle, $haystack, $expectedResult): void
-    {
-        // Unit test code
+    #[DataProvider('filterByValueRecursive')]
+    #[Test]
+    public function filterByValueRecursiveCorrectlyFiltersArray(
+        $needle,
+        $haystack,
+        $expectedResult,
+    ): void {
+        self::assertEquals(
+            $expectedResult,
+            ArrayUtility::filterByValueRecursive($needle, $haystack),
+        );
     }
 }
