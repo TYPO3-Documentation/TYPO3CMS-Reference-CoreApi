038dc9ceef14a64c65406678cd1256badf6090ee
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-06-07 18:19:33 +0200

    [TASK] Move LanguageService example to PHP file (#4500)
    Resolve syntax errors and apply CGL.
    
    Follow up of https://github.com/TYPO3-Documentation/TYPO3CMS-Reference-CoreApi/pull/4477
    
    Releases: main, 12.4

---
+++ Documentation/ApiOverview/Localization/LocalizationApi/_ExampleController.php
@@ -0,0 +1,40 @@
+<?php
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Core\Localization\LanguageServiceFactory;
+
+final class ExampleController
+{
+    private ServerRequestInterface $request;
+
+    public function __construct(
+        private readonly LanguageServiceFactory $LanguageServiceFactory,
+    ) {}
+
+    public function processAction(
+        string $content,
+        array $configurations,
+        ServerRequestInterface $request,
+    ): string {
+        $this->request = $request;
+
+        // ...
+        $content .=  $this->getTranslatedLabel(
+            'LLL:EXT:my_extension/Resources/Private/Language/locallang.xlf:labels.exampleLabel',
+        );
+        // ...
+
+        return $content;
+    }
+
+    private function getTranslatedLabel(string $key): string
+    {
+        $language =
+            $this->request->getAttribute('language')
+                ?? $this->request->getAttribute('site')->getDefaultLanguage();
+        $languageService = $this->LanguageServiceFactory
+            ->createFromSiteLanguage($language);
+
+        return $languageService->sL($key);
+    }
+}
