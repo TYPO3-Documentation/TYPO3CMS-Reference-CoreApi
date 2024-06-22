dc162132e94dfe4dae2d79ab99c545c490214eac
lina.wolf <lwolf@w-commerce.de>
2024-06-22 08:03:27 +0200

    Feature: #102337 - PSR-14 event for modifying record list download presets
    References: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/934
    Releases: main

--- Documentation/ApiOverview/Events/Events/Backend/_BeforeRecordDownloadPresetsAreDisplayedEvent/_PresetListener.php
+++ Documentation/ApiOverview/Events/Events/Backend/_BeforeRecordDownloadPresetsAreDisplayedEvent/_PresetListener.php
@@ -13,24 +13,24 @@ final readonly class PresetListener
 {
     public function __invoke(BeforeRecordDownloadPresetsAreDisplayedEvent $event): void
     {
-    $presets = $event->getPresets();
+        $presets = $event->getPresets();
 
-    $newPresets = match ($event->getDatabaseTable()) {
-        'be_users' => [new DownloadPreset('PSR-14 preset', ['uid', 'email'])],
-        'pages' => [
-            new DownloadPreset('PSR-14 preset', ['title']),
-            new DownloadPreset('Another PSR-14 preset', ['title', 'doktype']),
-        ],
-        'tx_myvendor_myextension' => [new DownloadPreset('PSR-14 preset', ['uid', 'something'])],
-    };
+        $newPresets = match ($event->getDatabaseTable()) {
+            'be_users' => [new DownloadPreset('PSR-14 preset', ['uid', 'email'])],
+            'pages' => [
+                new DownloadPreset('PSR-14 preset', ['title']),
+                new DownloadPreset('Another PSR-14 preset', ['title', 'doktype']),
+            ],
+            'tx_myvendor_myextension' => [new DownloadPreset('PSR-14 preset', ['uid', 'something'])],
+        };
 
-    foreach ($newPresets as $newPreset) {
-        $presets[] = $newPreset;
-    }
+        foreach ($newPresets as $newPreset) {
+            $presets[] = $newPreset;
+        }
 
-    $presets[] = new DownloadPreset('Available everywhere, simple UID list', ['uid']);
-    $presets['some-identifier'] = new DownloadPreset('Overwrite preset', ['uid', 'pid'], 'some-identifier');
+        $presets[] = new DownloadPreset('Available everywhere, simple UID list', ['uid']);
+        $presets['some-identifier'] = new DownloadPreset('Overwrite preset', ['uid', 'pid'], 'some-identifier');
 
-    $event->setPresets($presets);
+        $event->setPresets($presets);
     }
 }
