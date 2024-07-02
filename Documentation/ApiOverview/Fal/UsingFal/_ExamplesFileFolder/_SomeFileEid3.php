180500e7104ba0b9483ea705162a6be37ab7bb2b
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-07-02 22:23:56 +0200

    [TASK] Deprecate GeneralUtility::hmac() (#4584)
    * [TASK] Deprecate GeneralUtility::hmac()
    
    This is the only mentioning of GeneralUtility::hmac
    
    resovles: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/882
    releases: main
    
    * [TASK] Deprecate GeneralUtility::hmac()
    
    This is the only mentioning of GeneralUtility::hmac
    
    resovles: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/882
    releases: main
    
    * Update ExamplesFileFolder.rst
    
    ---------
    
    Co-authored-by: Garvin Hicking <blog@garv.in>

--- Documentation/ApiOverview/Fal/UsingFal/_ExamplesFileFolder/_SomeFileEid3.php
+++ Documentation/ApiOverview/Fal/UsingFal/_ExamplesFileFolder/_SomeFileEid3.php
@@ -6,16 +6,21 @@ namespace MyVendor\MyExtension\Service;
 
 use MyVendor\MyExtension\Domain\Model\SomeModel;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Crypto\HashService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 
 class SomeClass
 {
+    public function __construct(private readonly HashService $hashService) {}
     public function getPublicUrl(SomeModel $resourceObject): string
     {
         $queryParameterArray = ['eID' => 'dumpFile', 't' => 'p'];
         $queryParameterArray['p'] = $resourceObject->getUid();
-        $queryParameterArray['token'] = GeneralUtility::hmac(implode('|', $queryParameterArray), 'resourceStorageDumpFile');
+        $queryParameterArray['token'] = $this->hashService->hmac(
+            implode('|', $queryParameterArray),
+            'resourceStorageDumpFile',
+        );
         $publicUrl = GeneralUtility::locationHeaderUrl(PathUtility::getAbsoluteWebPath(Environment::getPublicPath() . '/index.php'));
         $publicUrl .= '?' . http_build_query($queryParameterArray, '', '&', PHP_QUERY_RFC3986);
         return $publicUrl;
