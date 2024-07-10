a9294cc055d47423f450ffc9e99660917c4b67e7
lina.wolf <lwolf@w-commerce.de>
2024-07-10 15:45:29 +0200

    [TASK] Add an example on how to migrate via upgrade script from switchable controller actions
    releases: main, 12.4

---
+++ Documentation/ExtensionArchitecture/HowTo/UpdateExtensions/UpdateWizards/_SwitchableControllerActionUpgradeWizard.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace MyVendor\MyExtension\Upgrades;
+
+use Doctrine\DBAL\Exception;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Service\FlexFormService;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+use TYPO3\CMS\Install\Updates\RepeatableInterface;
+use TYPO3\CMS\Install\Updates\UpgradeWizardInterface;
+
+#[UpgradeWizard('myExtension_switchableControllerActionUpgradeWizard')]
+final class SwitchableControllerActionUpgradeWizard implements UpgradeWizardInterface
+{
+    private const TABLE = 'tt_content';
+    private const PLUGIN = 'myextension_myplugin';
+    public function __construct(
+        private readonly ConnectionPool $connectionPool,
+        private readonly FlexFormService $flexFormService
+    ) {
+    }
+
+    public function executeUpdate(): bool
+    {
+        $result = 0;
+        $result += $this->migratePlugin(self::PLUGIN, 'Videoplayer->list', 'ableplayer_videoplayerlist');
+        $result += $this->migratePlugin(self::PLUGIN, 'Videoplayer->overview;Videoplayer->detail', 'ableplayer_audioplayeroverviewdetail');
+        return $result > 0;
+    }
+
+    private function migratePlugin(string $plugin, string $switchable, string $newCType): int
+    {
+        $updated = 0;
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::TABLE);
+        $result = $queryBuilder
+            ->select('*')
+            ->from(self::TABLE)
+            ->where(
+                $queryBuilder->expr()->eq('list_type', $queryBuilder->createNamedParameter($plugin))
+            )
+            ->executeQuery();
+        while ($row = $result->fetchAssociative()) {
+            if (!is_string($row['pi_flexform']??false)) {
+                continue;
+            }
+            $flexform = $this->loadFlexForm($row['pi_flexform']);
+            if (!isset($flexform['switchableControllerActions']) || $flexform['switchableControllerActions'] !== $switchable) {
+                continue;
+            }
+            $updated++;
+            $this->connectionPool->getConnectionForTable('tt_content')
+                ->update(
+                    self::TABLE,
+                    [ // set
+                        'CType' => $newCType,
+                        'list_type' => '',
+                    ],
+                    [ 'uid' => (int)$row['uid'] ], // where
+                );
+        }
+        return $updated;
+    }
+    private function loadFlexForm(string $flexFormString): array
+    {
+        return $this->flexFormService
+            ->convertFlexFormContentToArray($flexFormString);
+    }
+
+    public function updateNecessary(): bool
+    {
+        $queryBuilder = $this->connectionPool->getQueryBuilderForTable(self::TABLE);
+        $queryBuilder
+            ->count('uid')
+            ->from(self::TABLE)
+            ->where(
+                $queryBuilder->expr()->eq('list_type', self::PLUGIN)
+            )
+            ->executeQuery()
+            ->fetchOne();
+        return true;
+    }
+
+    public function getTitle(): string
+    {
+        return 'Migrate MyExtension plugins';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Migrate MyExtension plugins from switchable controller actions to specific plugins';
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getPrerequisites(): array
+    {
+        return [];
+    }
+}
