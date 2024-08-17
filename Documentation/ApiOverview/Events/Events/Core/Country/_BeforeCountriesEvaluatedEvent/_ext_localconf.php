10d337a932226f09a5c3bd1ae6812308e4d2995e
lina.wolf <lwolf@w-commerce.de>
2024-08-17 08:13:53 +0200

    [FEATURE] Add PSR-14 event to modify available countries
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/985
    Releases: main

---
+++ Documentation/ApiOverview/Events/Events/Core/Country/_BeforeCountriesEvaluatedEvent/_ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']
+['EXT:core/Resources/Private/Language/Iso/countries.xlf'][]
+    = 'EXT:my_extension/Resources/Private/Language/countries.xlf';
