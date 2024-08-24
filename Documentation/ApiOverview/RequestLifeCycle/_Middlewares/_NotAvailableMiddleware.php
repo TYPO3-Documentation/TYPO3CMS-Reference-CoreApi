d0ad40ddc27166917a81ed88f9f1abbe4beabdea
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-08-24 07:08:23 +0200

    [TASK] Middleware: Move code snippets to their own files (#4696)
    * [TASK] Middleware: Move code snippets to their own files
    
    And adjust indentation
    
    * [TASK] Middleware: Move code snippets to their own files
    
    And adjust indentation

---
+++ Documentation/ApiOverview/RequestLifeCycle/_Middlewares/_NotAvailableMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\ErrorController;
+
+class NotAvailableMiddleware implements MiddlewareInterface
+{
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler,
+    ): ResponseInterface {
+        if ($request->getRequestTarget() === 'foo/bar') {
+            return GeneralUtility::makeInstance(ErrorController::class)
+                ->unavailableAction(
+                    $request,
+                    'This page is temporarily unavailable.',
+                );
+        }
+
+        return $handler->handle($request);
+    }
+}
---
+++ Documentation/ApiOverview/RequestLifeCycle/_Middlewares/_RequestEnrichingMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Routing\RouterInterface;
+
+class RequestEnrichingMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private readonly RouterInterface $matcher,
+    ) {}
+
+    public function process(
+        ServerRequestInterface $request,
+        RequestHandlerInterface $handler,
+    ): ResponseInterface {
+        $routeResult = $this->matcher->matchRequest($request);
+
+        $request = $request->withAttribute('site', $routeResult->getSite());
+        $request = $request->withAttribute('language', $routeResult->getLanguage());
+
+        return $handler->handle($request);
+    }
+}
