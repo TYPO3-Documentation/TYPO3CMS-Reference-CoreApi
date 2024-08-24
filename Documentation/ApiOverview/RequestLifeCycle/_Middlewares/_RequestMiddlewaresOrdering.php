7d5bdccb5dfe4ccce57152c0f8c020350f0e232b
lina.wolf <lwolf@w-commerce.de>
2024-08-24 07:05:17 +0200

    [TASK] Middleware: Move code snippets to their own files
    And adjust indentation

---
+++ Documentation/ApiOverview/RequestLifeCycle/_Middlewares/_RequestMiddlewaresOrdering.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'frontend' => [
+        'middleware-identifier' => [
+            'after' => [
+                'another-middleware-identifier',
+            ],
+            'before' => [
+                '3rd-middleware-identifier',
+            ]
+        ],
+    ]
+];
