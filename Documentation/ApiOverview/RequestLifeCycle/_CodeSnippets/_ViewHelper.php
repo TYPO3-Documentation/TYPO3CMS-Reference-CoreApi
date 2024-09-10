6ae607cf14a57fc520b6942d180dc59324902b91
lina.wolf <lwolf@w-commerce.de>
2024-09-10 11:12:54 +0200

    [TASK] Use fluid attribute for Request handling
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/995
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1006
    Releases: main

---
+++ Documentation/ApiOverview/RequestLifeCycle/_CodeSnippets/_ViewHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\ViewHelpers;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+final class MyViewHelper extends AbstractViewHelper
+{
+    public function render(): string
+    {
+        $request = $this->getRequest($this->renderingContext);
+        return $request !== null ? 'Request found' : 'No request found';
+    }
+
+    private function getRequest(): ServerRequestInterface|null
+    {
+        if ($this->renderingContext->hasAttribute(ServerRequestInterface::class)) {
+            return $this->renderingContext->getAttribute(ServerRequestInterface::class);
+        }
+        return null;
+    }
+}
