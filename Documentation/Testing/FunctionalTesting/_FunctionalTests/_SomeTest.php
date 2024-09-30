7bb3b1535413cdf958c9bf31336715b9af626795
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-09-30 09:11:31 +0200

    [Backport 12.4][TASK] Update functional tests examples (#4784)
    Mainly:
    - Remove superfluous "Test Case" comment
    - Add `declare(strict_types=1);` as best practise
    - Use `@test` annotation as best practise like the Core does
    - Add `void` return type as best practise
    - Add `typo3/testing-framework` as requirement to have code completion in the PHP files
    
    Releases: main, 12.4

--- Documentation/Testing/FunctionalTesting/_FunctionalTests/_SomeTest.php
+++ Documentation/Testing/FunctionalTesting/_FunctionalTests/_SomeTest.php
@@ -1,19 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MyVendor\MyExtension\Tests\Functional;
 
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
-/**
- * Test case
- */
 class SomeTest extends FunctionalTestCase
 {
     protected array $coreExtensionsToLoad = [
         'workspaces',
     ];
 
-    public function testSomethingWithWorkspaces()
+    /**
+     * @test
+     */
+    public function somethingWithWorkspaces(): void
     {
         //...
     }
--- Documentation/Testing/FunctionalTesting/_FunctionalTests/_SomeTestFiles.php
+++ Documentation/Testing/FunctionalTesting/_FunctionalTests/_SomeTestFiles.php
@@ -1,18 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MyVendor\MyExtension\Tests\Functional;
 
 use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
 
-/**
- * Test case
- */
 class SomeTest extends FunctionalTestCase
 {
     protected array $pathsToLinkInTestInstance = [
         'typo3/sysext/impexp/Tests/Functional/Fixtures/Folders/fileadmin/user_upload/typo3_image2.jpg' => 'fileadmin/user_upload/typo3_image2.jpg',
     ];
-    public function testSomethingWithFiles()
+
+    /**
+     * @test
+     */
+    public function somethingWithFiles(): void
     {
         //...
     }
