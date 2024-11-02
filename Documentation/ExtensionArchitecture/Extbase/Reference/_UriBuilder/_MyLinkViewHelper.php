4e0c551cc15d2a2efeb64e5a0ae2b32eea67ec87
Garvin Hicking <blog@garv.in>
2024-11-02 10:21:10 +0100

    [TASK] Explain UriBuilder usage in Extbase ViewHelpers

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/_UriBuilder/_MyLinkViewHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller\MyController;
+
+use Psr\Http\Message\ServerRequestInterface;
+use TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+final class MyLinkViewHelper extends AbstractViewHelper
+{
+    public function __construct(private UriBuilder $uriBuilder)
+    {
+    }
+
+    public function render(): string
+    {
+        if ($this->renderingContext->hasAttribute(ServerRequestInterface::class)) {
+            $request = $this->renderingContext->getAttribute(ServerRequestInterface::class);
+        } else {
+            throw new \RuntimeException(
+                'The rendering context of this ViewHelper is missing a valid request object, probably because it is used outside of Extbase context.',
+                1730537505
+            );
+        }
+
+        // Request context is needed before $this->uriBuilder is first used for returning links.
+        // Note: this will not be reset on calling $this->uriBuilder->reset()!
+        $this->uriBuilder->setRequest($request);
+
+        $url = $this->uriBuilder
+            ->reset()
+            ->setTargetPageUid(2751)
+            ->uriFor(
+                'anotherAction',
+                [
+                    'myRecord' => 21,
+                ],
+                'MyController',
+                'myextension',
+                'myplugin',
+            );
+
+        // do something with $url, for example:
+        return 'Link: ' . $url . '</a>';
+    }
+}
