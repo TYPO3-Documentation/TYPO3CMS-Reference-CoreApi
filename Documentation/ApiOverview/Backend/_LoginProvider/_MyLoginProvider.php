cf1ac040664b435c7831f281bb858263ab2e3f87
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2024-12-05 10:36:40 +0100

    [DOCS] Improve LoginProviderInterface->modifyView() ReST (#5113)
    * [DOCS] Improve LoginProviderInterface->modifyView() ReST
    
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1056
    Releases: main, 13.4
    
    * Apply suggestions from code review
    
    Co-authored-by: Stefan Frömken <froemken@gmail.com>
    
    ---------
    
    Co-authored-by: Stefan Frömken <froemken@gmail.com>

--- Documentation/ApiOverview/Backend/_LoginProvider/_MyLoginProvider.php
+++ Documentation/ApiOverview/Backend/_LoginProvider/_MyLoginProvider.php
@@ -7,6 +7,7 @@ use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
 use TYPO3\CMS\Backend\LoginProvider\LoginProviderInterface;
 use TYPO3\CMS\Core\Page\PageRenderer;
 use TYPO3\CMS\Core\View\ViewInterface;
+use TYPO3\CMS\Fluid\View\FluidViewAdapter;
 
 #[Autoconfigure(public: true)]
 final readonly class MyLoginProvider implements LoginProviderInterface
@@ -18,6 +19,13 @@ final readonly class MyLoginProvider implements LoginProviderInterface
     public function modifyView(ServerRequestInterface $request, ViewInterface $view): string
     {
         $this->pageRenderer->addJsFile('someFile');
+        // Custom login provider implementations can add custom fluid lookup paths.
+        if ($view instanceof FluidViewAdapter) {
+            $templatePaths = $view->getRenderingContext()->getTemplatePaths();
+            $templateRootPaths = $templatePaths->getTemplateRootPaths();
+            $templateRootPaths[] = 'EXT:my_extension/Resources/Private/Templates';
+            $templatePaths->setTemplateRootPaths($templateRootPaths);
+        }
         $view->assign('Some Variable', 'some value');
         return 'Login/MyLoginForm';
     }
