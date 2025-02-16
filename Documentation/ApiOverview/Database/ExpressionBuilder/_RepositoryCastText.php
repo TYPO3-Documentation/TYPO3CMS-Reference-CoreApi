e4ca1d2e722b0daa9d12204a30b9b74940b15339
lina.wolf <lwolf@w-commerce.de>
2025-02-16 18:10:35 +0100

    [FEATURE] Add castText() expression support to ExpressionBuilder
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/978
    Releases: main, 13.4

---
+++ Documentation/ApiOverview/Database/ExpressionBuilder/_RepositoryCastText.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Domain\Repository;
+
+use TYPO3\CMS\Core\Database\ConnectionPool;
+
+final class MyTableRepository
+{
+    private const TABLE_NAME = 'my_table';
+
+    public function __construct(private readonly ConnectionPool $connectionPool)
+    {
+    }
+
+    public function demonstrateCastText(string $col1, string $col2): array
+    {
+        $queryBuilder = $this->connectionPool
+            ->getQueryBuilderForTable(self::TABLE_NAME);
+
+        return $queryBuilder
+            ->selectLiteral(
+                $queryBuilder->quoteIdentifier($col1),
+                $queryBuilder->quoteIdentifier($col2),
+                // !!! Escape all values passed to castText to prevent SQL injections
+                $queryBuilder->expr()->castText(
+                    $queryBuilder->quoteIdentifier($col2) . ' * 10',
+                    'virtual_field'
+                )
+            )
+            ->from(self::TABLE_NAME)
+            ->executeQuery()
+            ->fetchAllAssociative();
+    }
+}
