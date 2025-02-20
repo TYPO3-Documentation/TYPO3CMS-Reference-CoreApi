2b48b017bc7c5d4688aa82d1aa459cc03bf4dfcf
lina.wolf <lwolf@w-commerce.de>
2025-02-20 16:14:22 +0100

    [TASK] Use confval to describe ext_emconf.php
    Releases: main, 13.4, 12.4

---
+++ Documentation/ExtensionArchitecture/FileStructure/_ExtEmconf/_ext_emconf_autoload.php
@@ -0,0 +1,11 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Extension title',
+    // ...
+    'autoload' => [
+        'classmap' => [
+            'Classes',
+            'a-class.php',
+        ]
+    ],
+];
---
+++ Documentation/ExtensionArchitecture/FileStructure/_ExtEmconf/_ext_emconf_constraints.php
@@ -0,0 +1,17 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Extension title',
+    // ...
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.4.99'
+        ],
+        'conflicts' => [
+            'tt_news' => ''
+        ],
+        'suggests' => [
+            'news' => '12.1.0-12.99.99'
+        ],
+    ]
+];
