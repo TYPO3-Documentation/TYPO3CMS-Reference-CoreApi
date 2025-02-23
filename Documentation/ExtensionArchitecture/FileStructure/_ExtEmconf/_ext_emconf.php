191378bf711aafeaba6d658d976ada56ab7db53a
lina.wolf <lwolf@w-commerce.de>
2025-02-23 08:18:48 +0100

    [TASK] Use confval to describe ext_emconf.php (#5361)
    * [TASK] Use confval to describe ext_emconf.php
    
    Releases: main, 13.4, 12.4
    
    * [TASK] Use confval to describe ext_emconf.php
    
    Releases: main, 13.4, 12.4
    
    (cherry picked from commit 5476592fe3a62f3de7ce3cfebecef39dee733294)

---
+++ Documentation/ExtensionArchitecture/FileStructure/_ExtEmconf/_ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Extension title',
+    'description' => 'Extension description',
+    'category' => 'plugin',
+    'author' => 'John Doe',
+    'author_email' => 'john.doe@example.org',
+    'author_company' => 'some company',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.4.0-13.4.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
