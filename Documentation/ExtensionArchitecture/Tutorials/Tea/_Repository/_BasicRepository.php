2bb727409a21dcdac8fd8f75925dc8d43843a464
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2025-03-03 08:27:57 +0100

    [TASK] Update TeaController and Repository (#5411)
    * [TASK] Update TeaController and Repository
    
    Resolves: #3864
    Releases: main, 13.4
    
    * [TASK] Update TeaController and Repository
    
    Resolves: #3864
    Releases: main, 13.4

---
+++ Documentation/ExtensionArchitecture/Tutorials/Tea/_Repository/_BasicRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTN\Tea\Domain\Repository\Product;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * @extends Repository<Tea>
+ */
+class TeaRepository extends Repository {}
