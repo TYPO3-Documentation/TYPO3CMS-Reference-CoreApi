44b8c2873c1480660daeac61681726bd8e02d632
lina.wolf <lwolf@w-commerce.de>
2025-03-29 07:46:46 +0100

    [TASK] Update codesnippets and remove deleted ones from generation
    Releases: main

--- Documentation/CodeSnippets/Config/Api/SessionManagement.php
+++ Documentation/CodeSnippets/Config/Api/SessionManagement.php
@@ -1,22 +1,4 @@
 <?php
 
 return [
-    [
-        'action' => 'createPhpClassDocs',
-        'class' => \TYPO3\CMS\Core\Session\SessionManager::class,
-        'targetFileName' => 'ApiOverview/SessionStorageFramework/_SessionManager.rst.txt',
-        'withCode' => false,
-    ],
-    [
-        'action' => 'createPhpClassDocs',
-        'class' => \TYPO3\CMS\Core\Session\UserSession::class,
-        'targetFileName' => 'ApiOverview/SessionStorageFramework/_UserSession.rst.txt',
-        'withCode' => false,
-    ],
-    [
-        'action' => 'createPhpClassDocs',
-        'class' => \TYPO3\CMS\Core\Session\UserSessionManager::class,
-        'targetFileName' => 'ApiOverview/SessionStorageFramework/_UserSessionManager.rst.txt',
-        'withCode' => false,
-    ],
 ];
