8ff4821ff9e03d4eda372b67df4487fddea055b4
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2025-05-02 08:34:25 +0200

    [FEATURE] Country as Extbase Type (#5567)
    * [FEATURE] Country as Extbase Type
    
    References: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1232
    Releases: main
    
    * [FEATURE] Country as Extbase Type
    
    References: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1232
    Releases: main

---
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/Model/PropertyTypes/_codesnippets/_country_tca.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    //...
+    'columns' => [
+        'country_of_origin' => [
+            'label' => 'Country of origin',
+            'config' => [
+                'type' => 'country',
+            ],
+        ],
+    ],
+];
