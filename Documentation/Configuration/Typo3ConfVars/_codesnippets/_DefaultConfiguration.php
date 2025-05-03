fef59a897fdaa0371aa0d706e76b5836baf10c0e
lina.wolf <lwolf@w-commerce.de>
2025-05-03 14:45:11 +0200

    [TASK] Merge and condense config overview chapters (#5569)
    * [TASK] Merge and condense config overview chapters
    
    As a preparation for including the
    site sets here
    
    Releases: main, 13.4, 12.4
    
    * [TASK] Language checks
    
    Releases: main
    
    ---------
    
    Co-authored-by: Sarah McCarthy <sarahmccarthy123@yahoo.com>
    
    (cherry picked from commit 38c5230b8e8821eb0ed6b6a01369a87f71f9452c)

---
+++ Documentation/Configuration/Typo3ConfVars/_codesnippets/_DefaultConfiguration.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'GFX' => [
+        'thumbnails' => true,
+        'thumbnails_png' => true,
+        'gif_compress' => true,
+        'imagefile_ext' => 'gif,jpg,jpeg,tif,tiff,bmp,pcx,tga,png,pdf,ai,svg',
+        // ...
+    ],
+    // ...
+];
