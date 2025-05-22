535bf70f6249a36fedbba59f21aa94c021cab0a9
lina.wolf <lwolf@w-commerce.de>
2025-05-22 10:36:53 +0200

    [TASK] Why docker only

--- Documentation/Administration/Installation/ClassicMode/_codesnippets/_DockerDirDemo.sh
+++ Documentation/Administration/Installation/ClassicMode/_codesnippets/_DockerDirDemo.sh
@@ -1,5 +1,5 @@
 mkdir -p typo3demo
 cd typo3demo
 mkdir -p fileadmin typo3conf typo3temp
-# Ensure TYPO3 can write to these directories
-chmod -R 777 fileadmin typo3conf typo3temp
+# On Linux and WSL during development: Ensure TYPO3 can write to these directories
+# chmod -R 777 fileadmin typo3conf typo3temp
