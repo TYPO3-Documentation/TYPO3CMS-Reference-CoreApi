cb2de8f58ee1ad577f54d25350f6e71fb9598489
lina.wolf <lwolf@w-commerce.de>
2025-05-23 13:47:18 +0200

    [TASK] Update more admin Security Guidelines
    Releases: main, 13.4. 12.4

---
+++ Documentation/Security/GuidelinesAdministrators/_codesnippets/_additional.php
@@ -0,0 +1,15 @@
+<?php
+
+use Psr\Log\LogLevel;
+use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Log\Writer\FileWriter;
+
+// Other settings
+
+$GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['Core']['Authentication']['writerConfiguration'] = [
+    LogLevel::INFO => [
+        FileWriter::class => [
+            'logFile' => Environment::getVarPath() . '/log/typo3_auth.log',
+        ]
+    ]
+];
