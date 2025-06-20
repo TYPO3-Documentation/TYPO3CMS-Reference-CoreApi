438662999641b4ab9e80a0183b063ce754ab988a
lina.wolf <lwolf@w-commerce.de>
2025-06-20 10:52:00 +0200

    [TASK] Update code snippets
    Releases: main

--- Documentation/Administration/Production/Logging/_codesnippets/_disableDeprecationLog.php
+++ Documentation/Administration/Production/Logging/_codesnippets/_disableDeprecationLog.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use TYPO3\CMS\Core\Core\Environment;
 
 // Add this at the very bottom of your system/additional.php
-// Or your site packages ext_localconf.php to override settings 
+// Or your site packages ext_localconf.php to override settings
 // made by other extensions
 if (Environment::getContext()->isProduction()) {
     unset($GLOBALS['TYPO3_CONF_VARS']['LOG']['TYPO3']['CMS']['deprecations']);
