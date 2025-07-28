946698207dbbf8ba9be20ce02ed7361a4fe658b6
lina.wolf <lwolf@w-commerce.de>
2025-07-28 20:38:18 +0200

    [DOCS] Make registration of commands with attributes standard
    And refactor the examples and chapter
    
    Releases: main, 13.4, 12.4

--- Documentation/ApiOverview/CommandControllers/_Tutorial/_CrazyCalculatorCommand.php
+++ Documentation/ApiOverview/CommandControllers/_Tutorial/_CrazyCalculatorCommand.php
@@ -3,19 +3,21 @@
 declare(strict_types=1);
 
 namespace MyVendor\MyExtension\Command;
+
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+
 #[AsCommand(
-    name: 'myextension:crazycalculator'
+    name: 'myextension:crazycalculator',
 )]
 final class CrazyCalculatorCommand extends Command
 {
     protected function execute(
         InputInterface $input,
-        OutputInterface $output
+        OutputInterface $output,
     ): int {
         $io = new SymfonyStyle($input, $output);
         $io->title('Welcome to our awesome extension');
@@ -33,11 +35,11 @@ final class CrazyCalculatorCommand extends Command
         $operation = (string)$io->choice(
             'Chose the desired operation',
             ['squared', 'divided by 0'],
-            'squared'
+            'squared',
         );
         switch ($operation) {
             case 'squared':
-                $io->success(sprintf("%d squared is %d", $number, $number * $number));
+                $io->success(sprintf('%d squared is %d', $number, $number * $number));
                 return Command::SUCCESS;
             default:
                 $io->error('Operation ' . $operation . 'is not supported. ');
