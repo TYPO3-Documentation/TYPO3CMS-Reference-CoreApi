493a083f75076acf80ad5fb78a060a4f712e85b3
lina.wolf <lwolf@w-commerce.de>
2025-09-06 11:11:01 +0200

    [FEATURE] Introduce Fluid page title ViewHelper
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1308
    
    Releases: main

--- Documentation/ApiOverview/Seo/_PageTitleProvider/_RecordTitleProviderUsage.php
+++ Documentation/ApiOverview/Seo/_PageTitleProvider/_RecordTitleProviderUsage.php
@@ -6,15 +6,14 @@ namespace MyVendor\MyExtension\Controller;
 
 use MyVendor\MyExtension\Domain\Model\Item;
 use Psr\Http\Message\ResponseInterface;
-use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Core\PageTitle\RecordTitleProvider;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 final class ItemController extends ActionController
 {
     public function __construct(
-        private readonly RecordTitleProvider $recordTitleProvider
-    ) {
-    }
+        private readonly RecordTitleProvider $recordTitleProvider,
+    ) {}
 
     public function showAction(Item $item): ResponseInterface
     {
