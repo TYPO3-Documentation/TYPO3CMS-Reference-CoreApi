6d003e0ec2a4ed6f7fa295797cf11bbb59aa1678
lina.wolf <lwolf@w-commerce.de>
2025-09-07 12:58:20 +0200

    Correction from code review

--- Documentation/ApiOverview/Events/Events/Form/_BeforeFormIsSavedEvent/_MyEventListener.php
+++ Documentation/ApiOverview/Events/Events/Form/_BeforeFormIsSavedEvent/_MyEventListener.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace MyVendor\MyExtension\EventListener;
 
 use TYPO3\CMS\Core\Attribute\AsEventListener;
-use TYPO3\CMS\Form\Event\BeforeFormIsCreatedEvent;
+use TYPO3\CMS\Form\Event\BeforeFormIsSavedEvent;
 
 #[AsEventListener(
-    identifier: 'my-extension/before-form-is-created',
+    identifier: 'my-extension/before-form-is-saved',
 )]
 final readonly class MyEventListener
 {
-    public function __invoke(BeforeFormIsCreatedEvent $event): void
+    public function __invoke(BeforeFormIsSavedEvent $event): void
     {
         $event->form['label'] = 'foo';
     }
