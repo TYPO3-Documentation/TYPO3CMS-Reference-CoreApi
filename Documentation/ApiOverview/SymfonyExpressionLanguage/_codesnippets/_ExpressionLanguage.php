15de6b350ebdbe8a19dfbdb0670bf3158e4bf857
lina.wolf <lwolf@w-commerce.de>
2025-09-19 11:12:49 +0200

    [TASK] Update Symfony expression language
    * Move codesnippets to separate files
    * Apply CGL, best practices, resolve imports and namespaces
    * Unify the example file positions
    * Add anchors where missing
    * correct indentation
    
    Releases: main, 13.4

---
+++ Documentation/ApiOverview/SymfonyExpressionLanguage/_codesnippets/_ExpressionLanguage.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use MyVendor\MyExtension\TypoScript\CustomTypoScriptConditionProvider;
+
+return [
+    'typoscript' => [
+        CustomTypoScriptConditionProvider::class,
+    ],
+];
