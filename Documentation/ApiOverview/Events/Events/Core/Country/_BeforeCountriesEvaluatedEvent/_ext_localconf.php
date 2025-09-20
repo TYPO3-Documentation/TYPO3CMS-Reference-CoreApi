7b3973d0c873250f7e15f209bf7a24049bcee350
lina.wolf <lwolf@w-commerce.de>
2025-09-20 07:48:14 +0200

    [!!!][FEATURE] Use symfony/translation for translatable labels
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1317
    Releases: main

--- Documentation/ApiOverview/Events/Events/Core/Country/_BeforeCountriesEvaluatedEvent/_ext_localconf.php
+++ Documentation/ApiOverview/Events/Events/Core/Country/_BeforeCountriesEvaluatedEvent/_ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']
+$GLOBALS['TYPO3_CONF_VARS']['LANG']['resourceOverrides']
 ['EXT:core/Resources/Private/Language/Iso/countries.xlf'][]
     = 'EXT:my_extension/Resources/Private/Language/countries.xlf';
