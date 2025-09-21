db5cb72aa64a0b149070f180535be93a00d388c3
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2025-09-21 08:21:03 +0200

    [FEATURE] Introduce scheduler task wizard (#6049)
    * [FEATURE] Introduce scheduler task wizard
    
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1323
    Releases: main
    
    * Update Documentation/ApiOverview/Events/Events/Scheduler/ModifyNewSchedulerTaskWizardItemsEvent.rst
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * Update Documentation/ApiOverview/Events/Events/Scheduler/_ModifyNewSchedulerTaskWizardItemsEvent/_MyEventListener.php
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Events/Events/Scheduler/_ModifyNewSchedulerTaskWizardItemsEvent/_MyEventListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace MyVendor\MyExtension\EventListener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Scheduler\Event\ModifyNewSchedulerTaskWizardItemsEvent;
+
+final readonly class ModifySchedulerTaskWizardListener
+{
+    #[AsEventListener('my-extension/scheduler/modify-wizard-items')]
+    public function __invoke(ModifyNewSchedulerTaskWizardItemsEvent $event): void
+    {
+        // Add a custom task to the wizard
+        $event->addWizardItem('my_custom_task', [
+            'title' => 'My Custom Task',
+            'description' => 'A custom task provided by my extension',
+            'iconIdentifier' => 'my-custom-icon',
+            'taskType' => 'MyVendor\\MyExtension\\Task\\CustomTask',
+            'taskClass' => 'MyVendor\\MyExtension\\Task\\CustomTask',
+        ]);
+
+        // Remove an existing task
+        $event->removeWizardItem('redirects_redirects:checkintegrity');
+
+        // Modify existing wizard items
+        $wizardItems = $event->getWizardItems();
+        foreach ($wizardItems as $key => $item) {
+            if (isset($item['title']) && str_contains($item['title'], 'referenceindex:update')) {
+                $item['title'] = 'Update reference index';
+                $event->addWizardItem($key, $item);
+            }
+        }
+    }
+}
---
+++ Documentation/CodeSnippets/Config/Api/Events/EventsScheduler.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    [
+        'action' => 'createPhpClassDocs',
+        'class' => \TYPO3\CMS\Scheduler\Event\ModifyNewSchedulerTaskWizardItemsEvent::class,
+        'targetFileName' => 'CodeSnippets/Events/Scheduler/ModifyNewSchedulerTaskWizardItemsEvent.rst.txt',
+        'withCode' => false,
+    ],
+];
