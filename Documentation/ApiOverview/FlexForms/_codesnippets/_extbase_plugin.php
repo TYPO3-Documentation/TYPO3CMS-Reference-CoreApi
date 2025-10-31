490c4a8f9f8255f79c3f2faa7186551a47dc8ec8
Michael Telgkamp <michael.telgkamp@mindscreen.de>
2025-10-31 11:57:56 +0100

    [BUGFIX] Example for v13 had parameter introduced in v14 (#6143)
    The parameter `flexForm` for `registerPlugin` was added in 14.0.
    The code of the example was not valid for 13.4.
    
    References:
    https://docs.typo3.org/c/typo3/cms-core/main/en-us//Changelog/14.0/Feature-107047-FlexFormEnhancements.html#feature-107047-flexform-enhancements-direct-plugin-registration-and-raw-tca-support
    https://review.typo3.org/c/Packages/TYPO3.CMS/+/87161
    https://forge.typo3.org/issues/107047

--- Documentation/ApiOverview/FlexForms/_codesnippets/_extbase_plugin.php
+++ Documentation/ApiOverview/FlexForms/_codesnippets/_extbase_plugin.php
@@ -10,7 +10,6 @@ $ctypeKey = ExtensionUtility::registerPlugin(
     'my-extension-icon',
     'plugins',
     'Plugin description',
-    'FILE:EXT:my_extension/Configuration/FlexForms/MyFlexform.xml',
 );
 
 ExtensionManagementUtility::addToAllTCAtypes(
