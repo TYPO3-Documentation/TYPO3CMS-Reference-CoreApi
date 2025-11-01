3be92dfc4a3d475216b13812e572769ddc31a7d3
lina.wolf <lwolf@w-commerce.de>
2025-11-01 07:37:18 +0100

    [FEATURE] Support username for authentication in Redis cache backend
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1399
    Releases: main

--- Documentation/ApiOverview/CachingFramework/Backends/_redis_password.php
+++ Documentation/ApiOverview/CachingFramework/Backends/_redis_password.php
@@ -4,10 +4,10 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pages']['b
     = \TYPO3\CMS\Core\Cache\Backend\RedisBackend::class;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pages']['options']
     = [
-    'defaultLifetime' => 86400,
-    'database' => 0,
-    'hostname' => 'redis',
-    'port' => 6379,
-    'username' => 'redis',
-    'password' => 'redis',
-];
+        'defaultLifetime' => 86400,
+        'database' => 0,
+        'hostname' => 'redis',
+        'port' => 6379,
+        'username' => 'redis',
+        'password' => 'redis',
+    ];
