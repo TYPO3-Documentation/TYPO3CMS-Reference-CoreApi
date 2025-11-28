70c129562cec56d36992a223f6a1e85894785500
Chris Müller <coding@brotkrueml.dev>
2025-11-28 19:32:15 +0100

    Add PHP 8.5 to workflow

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -61,11 +61,12 @@ Options:
 
         If not specified, podman will be used if available. Otherwise, docker is used.
 
-    -p <8.2|8.3|8.4>
+    -p <8.2|8.3|8.4|8.5>
         Specifies the PHP minor version to be used
             - 8.2: (default) use PHP 8.2
             - 8.3: use PHP 8.3
             - 8.4: use PHP 8.4
+            - 8.5: use PHP 8.5
     -n
         Only with -s cgl, composerNormalize, rector
         Activate dry-run in CGL check and composer normalize that does not actively change files and only prints broken ones.
@@ -120,7 +121,7 @@ while getopts "b:s:p:xy:nhu" OPT; do
             ;;
         p)
             PHP_VERSION=${OPTARG}
-            if ! [[ ${PHP_VERSION} =~ ^(8.2|8.3|8.4)$ ]]; then
+            if ! [[ ${PHP_VERSION} =~ ^(8.2|8.3|8.4|8.5)$ ]]; then
                 INVALID_OPTIONS+=("p ${OPTARG}")
             fi
             ;;
