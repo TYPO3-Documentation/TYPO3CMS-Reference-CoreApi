eb29f2475d44171a59ef24b00174b286ce491ae2
Sarah McCarthy <sarahmccarthy123@yahoo.com>
2025-12-02 06:19:16 +0100

    [FEATURE] Add new form event BeforeRenderableIsValidatedEvent (#6176)
    * [FEATURE] Add new form event BeforeRenderableIsValidatedEvent
    
    Resolves: https://github.com/TYPO3-Documentation/Changelog-To-Doc/issues/1389
    Releases: main
    
    * Update Documentation/ApiOverview/Events/Events/Form/_BeforeRenderableIsValidatedEvent/_MyEventListener.php
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>
    
    * [TASK] Run make fix
    
    Releases: main
    
    ---------
    
    Co-authored-by: Chris Müller <2566282+brotkrueml@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Events/Events/Form/_BeforeRenderableIsValidatedEvent/_MyEventListener.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\EventListener;
+
+use TYPO3\CMS\Core\Attribute\AsEventListener;
+use TYPO3\CMS\Form\Event\BeforeRenderableIsValidatedEvent;
+
+final readonly class MyEventListener
+{
+    #[AsEventListener(
+        identifier: 'my-extension/before-renderable-is-validate',
+    )]
+    public function __invoke(BeforeRenderableIsValidatedEvent $event): void
+    {
+        $renderable = $event->renderable;
+        if ($renderable->getType() !== 'AdvancedPassword') {
+            return;
+        }
+
+        $elementValue = $event->value;
+        if ($elementValue['password'] !== $elementValue['confirmation']) {
+            $processingRule = $renderable->getRootForm()->getProcessingRule($renderable->getIdentifier());
+            $processingRule->getProcessingMessages()->addError(
+                GeneralUtility::makeInstance(
+                    Error::class,
+                    GeneralUtility::makeInstance(TranslationService::class)->translate('validation.error.1556283177', null, 'EXT:form/Resources/Private/Language/locallang.xlf'),
+                    1556283177,
+                ),
+            );
+        }
+        $event->value = $elementValue['password'];
+    }
+}
