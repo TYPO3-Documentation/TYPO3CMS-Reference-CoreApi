4f59f5554ff73163290c3d9312429f7ecc98da81
Florian Rival <florian.typo3@oktopuce.fr>
2026-01-12 12:13:51 +0100

    Correct the syntax for registring global namespace. (#6323)
    * Correct the syntax for registring global namespace.
    
    * Remove unused ViewHelpers import
    
    Removed unused import of MyVendor\MyExtension\ViewHelpers.
    
    * Add namespace configuration for myextension

--- Documentation/ApiOverview/Fluid/_Syntax/_ext_localconf.php
+++ Documentation/ApiOverview/Fluid/_Syntax/_ext_localconf.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use MyVendor\MyExtension\ViewHelpers;
-
 defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['myextension'] = [
-    ViewHelpers::class,
+    'MyVendor\\MyExtension\\ViewHelpers',
 ];
