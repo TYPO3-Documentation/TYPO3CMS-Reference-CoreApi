fbff6ea5abbc8d6cd7fcbc44e6193b3bf626623a
Florian Rival <florian.typo3@oktopuce.fr>
2026-01-12 12:06:21 +0100

    Remove unused ViewHelpers import
    Removed unused import of MyVendor\MyExtension\ViewHelpers.

--- Documentation/ApiOverview/Fluid/_Syntax/_ext_localconf.php
+++ Documentation/ApiOverview/Fluid/_Syntax/_ext_localconf.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use MyVendor\MyExtension\ViewHelpers;
-
 defined('TYPO3') or die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['blog'][] = 'MyVendor\BlogExample\ViewHelpers';
