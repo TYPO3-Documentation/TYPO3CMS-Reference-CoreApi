6d2da71511ee1b97052673017cbe1a2b6412afd9
Simon Schaufelberger <simonschaufi@users.noreply.github.com>
2026-01-23 16:25:14 +0100

    [TASK] Fix LLL_PREFIX path in EnumExample.php (#6349)
    Releases: main, 13.4

--- Documentation/ExtensionArchitecture/Extbase/Reference/Domain/Model/PropertyTypes/_codesnippets/_EnumExample.php
+++ Documentation/ExtensionArchitecture/Extbase/Reference/Domain/Model/PropertyTypes/_codesnippets/_EnumExample.php
@@ -9,7 +9,9 @@ enum Status: string
     case DRAFT = 'draft';
     case IN_REVIEW = 'in-review';
     case PUBLISHED = 'published';
-    public const LLL_PREFIX = 'LLL:EXT:my_extension/Resources/Private/Languages/locallang.xlf:status-';
+
+    public const LLL_PREFIX = 'LLL:EXT:my_extension/Resources/Private/Language/locallang.xlf:status-';
+
     public function getLabel(): string
     {
         return self::LLL_PREFIX . $this->value;
