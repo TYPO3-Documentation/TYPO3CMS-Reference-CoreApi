7fb726552bd8ee93c2d7c995814563e283e61591
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2026-05-14 06:15:56 +0200

    Use dependency injection in MetaTagManager examples (#6537)
    * Use dependency injection in MetaTagManager examples
    
    Releases: main, 14.3, 13.4
    
    * Update Documentation/ApiOverview/Seo/_MetaTagApi/_OgTitleRemoveAll.php
    
    Co-authored-by: Sandra Erbel <Alagts@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sandra Erbel <Alagts@users.noreply.github.com>

---
+++ Documentation/ApiOverview/Seo/_MetaTagApi/_OgTitle.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyVendor\MyExtension\Controller;
+
+use TYPO3\CMS\Core\MetaTag\MetaTagManagerRegistry;
+
+final class MyController
+{
+    public function __construct(
+        private readonly MetaTagManagerRegistry $metaTagManagerRegistry,
+    ) {}
+
+    public function addOgTitle(): void
+    {
+        $metaTagManager = $this->metaTagManagerRegistry->getManagerForProperty('og:title');
+        $metaTagManager->addProperty(
+            'og:title',
+            'This is the OG title from a controller',
+        );
+    }
+}
